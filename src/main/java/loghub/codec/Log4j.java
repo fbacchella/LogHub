e8d3ab0204c87596ad877c1f88961521c1a000c0
Fabrice Bacchella <fbacchella@spamcop.net>
2014-08-13 14:56:15 +0200

    Don't keep the LocationInformatin in log4j event if it's null

--- src/main/java/loghub/codec/Log4j.java
+++ src/main/java/loghub/codec/Log4j.java
@@ -8,10 +8,10 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.log4j.spi.LoggingEvent;
-
 import loghub.Codec;
 import loghub.Event;
+
+import org.apache.log4j.spi.LoggingEvent;
 
 public class Log4j extends Codec {
 
@@ -27,10 +27,13 @@ public class Log4j extends Codec {
             event.put("priority", o.getLevel());
             event.put("logger_name", o.getLoggerName());
             event.put("thread", o.getThreadName());
-            event.put("class", o.getLocationInformation().getClassName());
-            event.put("file", o.getLocationInformation().getFileName());
-            event.put("method", o.getLocationInformation().getMethodName());
-            event.put("line", o.getLocationInformation().getLineNumber());
+            o.getLocationInformation();
+            if(o.getLocationInformation().fullInfo != null) {
+                event.put("class", o.getLocationInformation().getClassName());
+                event.put("file", o.getLocationInformation().getFileName());
+                event.put("method", o.getLocationInformation().getMethodName());
+                event.put("line", o.getLocationInformation().getLineNumber());
+            }
             event.put("NDC", o.getNDC());
             if(o.getThrowableStrRep() != null) {
                 List<String> stack = new ArrayList<>();
