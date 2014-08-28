b2d26a983d4335c874344e6b00d99b5fb9a7e356
Fabrice Bacchella <fbacchella@spamcop.net>
2014-08-28 14:20:59 +0200

    Pipeline is now a useless class

--- src/main/java/loghub/transformers/Pipe.java
+++ src/main/java/loghub/transformers/Pipe.java
@@ -5,16 +5,35 @@ import java.util.Map;
 
 import loghub.Event;
 import loghub.PipeStep;
-import loghub.Pipeline;
 import loghub.SubPipeline;
 import loghub.Transformer;
+import loghub.ZMQManager;
+import loghub.ZMQManager.SocketInfo;
 
 public class Pipe extends Transformer {
 
+    private class Proxy {
+
+        private final String inEndpoint;
+        private final String outEndpoint;
+
+        Proxy(String parent, int rank, ZMQManager.Type inType, ZMQManager.Type outType) {
+            String name = "proxy-" + parent + "." + rank;
+            inEndpoint = "inproc://in." + parent + "." + rank;
+            outEndpoint = "inproc://out." + parent + "." + rank;
+            SocketInfo in = new SocketInfo(ZMQManager.Method.BIND, inType, inEndpoint);
+            SocketInfo out = new SocketInfo(ZMQManager.Method.BIND, outType, outEndpoint);
+            ZMQManager.proxy(name, in, out);
+        }
+
+    }
+
+
     private List<PipeStep[]> pipe;
-    Pipeline pipeline = null;
     SubPipeline child;
     final private String name;
+    private String inEndpoint = null;
+    private String outEndpoint = null;
 
     public Pipe(List<PipeStep[]> pipe, String name) {
         super();
@@ -32,37 +51,41 @@ public class Pipe extends Transformer {
     }
 
     public void startStream(Map<byte[], Event> eventQueue, String parent) {
-        pipeline = new Pipeline(eventQueue, parent, pipe);
+        Proxy[] proxies = new Proxy[pipe.size() + 1 ];
+        for(int i = 0; i <= pipe.size(); i++) {
+            ZMQManager.Type inType = i == 0 ? ZMQManager.Type.PULL : pipe.get(i-1)[0].getInType();
+            ZMQManager.Type outType = (i == pipe.size()) ? ZMQManager.Type.PUSH : pipe.get(i)[0].getOutType();
+            proxies[i] = new Proxy(parent, i + 1, inType , outType);
+        }
+        int i = 0;
+        for(PipeStep[] step: pipe) {
+            for(PipeStep p: step) {
+                p.start(eventQueue, proxies[i].outEndpoint, proxies[i+1].inEndpoint);
+            }
+            i++;
+        }
+        inEndpoint = proxies[0].inEndpoint;
+        outEndpoint = proxies[proxies.length - 1].outEndpoint;
     }
 
     public String getInEndpoint() {
-        return pipeline.getInEndpoint();
+        return inEndpoint;
     }
 
     public String getOutEndpoint() {
-        return pipeline.getOutEndpoint();
-    }
-
-    public Pipeline getPipeline() {
-        return pipeline;
+        return outEndpoint;
     }
 
     public PipeStep[] getPipeSteps() {
         if(child == null) {
-            child = new SubPipeline(name, pipeline.getInEndpoint(), pipeline.getOutEndpoint());            
+            child = new SubPipeline(name, inEndpoint, outEndpoint);            
         }
         return new PipeStep[] {child};
     }
 
     @Override
     public String toString() {
-        return name + "." + (pipe != null ? pipe.toString() : pipeline.toString());
-    }
-
-    public void stop() {
-        if(pipeline != null) {
-            pipeline.stop();            
-        }
+        return name + "." + (pipe != null ? pipe.toString() : inEndpoint + "->" + outEndpoint);
     }
 
 }
