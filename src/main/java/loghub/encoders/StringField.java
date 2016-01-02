83cb18345ae143796f2e6420aed451a0155f59d1
Fabrice Bacchella <fbacchella@spamcop.net>
2016-01-02 17:24:46 +0100

    Added a fields to string encoder

---
+++ src/main/java/loghub/encoders/StringField.java
@@ -0,0 +1,74 @@
+package loghub.encoders;
+
+import java.nio.charset.Charset;
+import java.util.Formatter;
+import java.util.Locale;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import loghub.Encode;
+import loghub.Event;
+import loghub.configuration.Beans;
+
+@Beans({"locale", "charset", "format"})
+public class StringField extends Encode {
+
+    private static final Pattern varregexp = Pattern.compile("(?<before>.*?)(?:\\$\\{(?<varname>[\\w\\.-]+)(?<format>%[^}]+)?\\})(?<after>.*)");
+
+    private Locale locale = Locale.getDefault();
+    private Charset charset = Charset.defaultCharset();
+    private String format = null;
+
+    private StringBuilder findVariables(Formatter f, StringBuilder buffer, String in, Map<String, Object> env) {
+        Matcher m = varregexp.matcher(in);
+        if(m.find()) {
+            String before = m.group("before");
+            String format = m.group("format");
+            String varname = m.group("varname");
+            String after = m.group("after");
+            buffer.append(before);
+            if(varname == null || ! varname.isEmpty()) {
+                if(format == null || format.isEmpty()) {
+                    format = "%s";
+                }
+                f.format(format, env.get(varname));
+
+            }
+            findVariables(f, buffer, after, env);
+        }
+        return buffer;
+    }
+
+    @Override
+    public byte[] encode(Event event) {
+        StringBuilder buffer = new StringBuilder();
+        findVariables(new Formatter(buffer, locale), buffer, format, event);
+        return buffer.toString().getBytes(charset);
+    }
+
+    public String getCharset() {
+        return charset.name();
+    }
+
+    public void setCharset(String charset) {
+        this.charset = Charset.forName(charset);
+    }
+
+    public String getLocale() {
+        return locale.toLanguageTag();
+    }
+
+    public void setLocale(String local) {
+        this.locale = Locale.forLanguageTag(local);
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+}
---
+++ src/test/java/loghub/encoders/TestStringField.java
@@ -0,0 +1,43 @@
+package loghub.encoders;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import loghub.Event;
+import loghub.LogUtils;
+import loghub.Tools;
+
+public class TestStringField {
+
+    private static Logger logger;
+
+    @BeforeClass
+    static public void configure() throws IOException {
+        Tools.configure();
+        logger = LogManager.getLogger();
+        LogUtils.setLevel(logger, Level.TRACE);
+    }
+
+    @Test
+    public void testone() {
+        StringField encoder = new StringField();
+        encoder.setCharset("UTF-16");
+        encoder.setFormat("${K1}: ${K2%02d}");
+        Event e = new Event();
+        e.put("K1", "V1");
+        e.put("K2", 2);
+
+        byte[] result = encoder.encode(e);
+
+        String formatted = new String(result, Charset.forName("UTF-16"));
+        Assert.assertEquals("Formatting failed", "V1: 02", formatted);
+    }
+
+}
