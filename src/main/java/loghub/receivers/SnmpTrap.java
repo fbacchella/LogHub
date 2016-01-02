5a6afa05167d8363f7df5828603b7d4ffb8e9ed5
Fabrice Bacchella <fbacchella@spamcop.net>
2016-01-02 13:13:10 +0100

    Improved SNMP trap receiver

--- src/main/java/loghub/receivers/SnmpTrap.java
+++ src/main/java/loghub/receivers/SnmpTrap.java
@@ -1,15 +1,21 @@
 package loghub.receivers;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Enumeration; 
+import java.util.Enumeration;
+import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Properties;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.snmp4j.CommandResponder;
 import org.snmp4j.CommandResponderEvent;
 import org.snmp4j.MessageDispatcherImpl;
@@ -29,47 +35,77 @@ import org.snmp4j.util.ThreadPool;
 
 import loghub.Event;
 import loghub.Receiver;
+import loghub.configuration.Beans;
 import loghub.snmp.NaturalOrderComparator;
 import loghub.snmp.OidTreeNode;
 
+@Beans({"protocol", "port", "listen"})
 public class SnmpTrap extends Receiver implements CommandResponder {
 
-    static public Snmp snmp;
-    static public ThreadPool threadPool;
-    static private OidTreeNode top;
-    static {
+    private static final Logger logger = LogManager.getLogger();
 
-        top = new OidTreeNode();
+    static private Snmp snmp;
+    static private ThreadPool threadPool;
+    static private final OidTreeNode top = new OidTreeNode();
+    static private boolean reconfigured = false;
 
+    private String protocol =  "udp";
+    private int port = 162;
+    private String listen = "0.0.0.0";
+
+    private static void loadOids(InputStream is) throws IOException {
         SortedMap<String, String> oids = new TreeMap<String, String>(new NaturalOrderComparator());
-        InputStream in = OidTreeNode.class.getClassLoader().getResourceAsStream("oid.properties");
         Properties p = new Properties();
-        try {
-            p.load(in);
-            for(Entry<Object, Object> e: p.entrySet()) {
-                oids.put((String) e.getKey(), (String) e.getValue());
-            }
-            for(Entry<String, String> e: oids.entrySet()) {
-                top.addOID(new OID(e.getKey()), e.getValue());                
-            }
-        } catch (IOException e) {
+        p.load(is);
+        for(Entry<Object, Object> e: p.entrySet()) {
+            oids.put((String) e.getKey(), (String) e.getValue());
+        }
+        for(Entry<String, String> e: oids.entrySet()) {
+            top.addOID(new OID(e.getKey()), e.getValue());
         }
     }
 
-    public SnmpTrap() {
-        super();
+    static {
+        InputStream in = OidTreeNode.class.getClassLoader().getResourceAsStream("oid.properties");
         try {
-            threadPool = ThreadPool.create("Trap", 10);
-            MultiThreadedMessageDispatcher dispatcher = new MultiThreadedMessageDispatcher(threadPool,
-                    new MessageDispatcherImpl());
-
-            Address listenAddress = GenericAddress.parse("udp:127.0.0.1/1162");
-            TransportMapping<?> transport = new DefaultUdpTransportMapping((UdpAddress) listenAddress);
-            snmp = new Snmp(dispatcher, transport);
-            snmp.getMessageDispatcher().addMessageProcessingModel(new MPv1());
-            snmp.getMessageDispatcher().addMessageProcessingModel(new MPv2c());
-            snmp.listen();
+            loadOids(in);
         } catch (IOException e) {
+            logger.fatal("unable to load default oid: {}", e.getMessage());
+            logger.catching(Level.DEBUG, e);
+        }
+    }
+
+    public SnmpTrap() throws IOException {
+        threadPool = ThreadPool.create("Trap", 2);
+        MultiThreadedMessageDispatcher dispatcher = new MultiThreadedMessageDispatcher(threadPool,
+                new MessageDispatcherImpl());
+        dispatcher.addCommandResponder(this);
+        Address listenAddress = GenericAddress.parse(protocol + ":" + listen + "/" + port);
+        TransportMapping<?> transport = new DefaultUdpTransportMapping((UdpAddress) listenAddress);
+        snmp = new Snmp(dispatcher, transport);
+        snmp.getMessageDispatcher().addMessageProcessingModel(new MPv1());
+        snmp.getMessageDispatcher().addMessageProcessingModel(new MPv2c());
+        snmp.listen();
+    }
+
+    @Override
+    public void configure(Map<String, Object> properties) {
+        super.configure(properties);
+
+        if(! reconfigured && properties.containsKey("oidfile")) {
+            reconfigured = true;
+            String oidfile = null;
+            try {
+                oidfile = (String) properties.get("oidfile");
+                InputStream is = new FileInputStream(oidfile);
+                loadOids(is);
+            } catch (ClassCastException e) {
+                logger.error("oidfile property is not a string");
+            } catch (FileNotFoundException e) {
+                logger.error("oidfile {} cant't be found", oidfile);
+            } catch (IOException e) {
+                logger.error("oidfile {} cant't be read:{}", oidfile, e.getMessage());
+            }
         }
     }
 
@@ -79,7 +115,6 @@ public class SnmpTrap extends Receiver implements CommandResponder {
             snmp.wait();
         } catch (InterruptedException e) {
         }
-        System.out.println("stop SnmpTrap");
     }
 
     @Override
@@ -89,24 +124,25 @@ public class SnmpTrap extends Receiver implements CommandResponder {
         Event event = new Event();
         for(VariableBinding i: Collections.list(vbenum)) {
             OID vbOID = i.getOid();
-            String key = SmartPrint(vbOID);
+            String key = smartPrint(vbOID);
             String value = i.toValueString();
             if(i.getVariable().getSyntax() == SMIConstants.SYNTAX_OBJECT_IDENTIFIER) {
-                value = SmartPrint( (OID) i.getVariable() );
+                value = smartPrint( (OID) i.getVariable() );
             }
             event.put(key, value);
         }
+        trap.setProcessed(true);
         send(event);
     }
 
-    public String SmartPrint(OID oid) {
+    public String smartPrint(OID oid) {
         OidTreeNode found = top.search(oid);
         OID foundOID = found.getOID();
         String formatted;
         if (oid.startsWith(foundOID)) {
             int[] suffixes = Arrays.copyOfRange(oid.getValue(), foundOID.size(), oid.size());
             if(suffixes.length > 0) {
-                formatted = found.getName() + "." +  new OID(suffixes);
+                formatted = found.getName() + "." + new OID(suffixes);
             }
             else {
                 formatted = found.getName();
@@ -121,6 +157,30 @@ public class SnmpTrap extends Receiver implements CommandResponder {
     @Override
     public String getReceiverName() {
         return "SnmpTrap";
+    }
+
+    public String getProtocol() {
+        return protocol;
+    }
+
+    public void setProtocol(String protocol) {
+        this.protocol = protocol;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getListen() {
+        return listen;
+    }
+
+    public void setListen(String listen) {
+        this.listen = listen;
     }
 
 }
