c1ab887776a4bacb5ea177853b99b900a9a14c72
Fabrice Bacchella <fbacchella@spamcop.net>
2016-01-04 22:15:50 +0100

    Added a VarExtractor transformer

---
+++ src/main/java/loghub/transformers/VarExtractor.java
@@ -0,0 +1,74 @@
+package loghub.transformers;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import loghub.Event;
+import loghub.Transformer;
+import loghub.configuration.Beans;
+
+/**
+ * This transformer parse a field using a regex that extract name and value.
+ * The regex must contains two named group name and value. The field is parsed until exhaustion. The unmatched content will stay in the field
+ * unless everything match, in this case, the field is removed.
+ * <p>
+ * The default parser is "(?&lt;name&gt;\p{Alnum}+)\p{Space}?[=:]\p{Space}?(?&lt;value&gt;[^;,:]+)[;,:]?" and should match most common case
+ * @author Fabrice Bacchella
+ *
+ */
+@Beans({"field", "parser"})
+public class VarExtractor extends Transformer {
+
+    private Pattern parser = Pattern.compile("(?<name>\\p{Alnum}+)\\p{Space}?[=:]\\p{Space}?(?<value>[^;,:]+)[;,:]?");
+    private String field = "message";
+    
+    @Override
+    public void transform(Event event) {
+        String fieldcontent = event.get(field).toString();
+        String after = fieldcontent;
+        Matcher m = parser.matcher(fieldcontent);
+        while(m.find()) {
+            String key = m.group("name");
+            String value = m.group("value");
+            if(key != null && ! key.isEmpty()) {
+                if(value != null && ! value.isEmpty()) {
+                    event.put(key, value);
+                }
+            }
+            after = fieldcontent.substring(m.end());
+        }
+        if(after != null && ! after.isEmpty()) {
+            event.put(field, after);
+        } else {
+            event.remove(field);
+        }
+    }
+
+    @Override
+    public String getName() {
+        return "VarExtractor";
+    }
+
+    /**
+     * @return the parser
+     */
+    public String getParser() {
+        return parser.pattern();
+    }
+
+    /**
+     * @param parser the parser to set
+     */
+    public void setParser(String parser) {
+        this.parser = Pattern.compile(parser);
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+
+}
---
+++ src/test/java/loghub/transformers/TestVarExtractor.java
@@ -0,0 +1,68 @@
+package loghub.transformers;
+
+import java.io.IOException;
+import java.util.Collection;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import loghub.Event;
+import loghub.LogUtils;
+import loghub.Tools;
+
+public class TestVarExtractor {
+
+    private static Logger logger;
+
+    @BeforeClass
+    static public void configure() throws IOException {
+        Tools.configure();
+        logger = LogManager.getLogger();
+        LogUtils.setLevel(logger, Level.TRACE, "loghub.transformers.Script");
+    }
+
+    @Test
+    public void test1() {
+        Event e = new Event();
+        e.put("message", "a=1;b:2;c");
+        VarExtractor t = new VarExtractor();
+        t.setField("message");
+        t.setParser("(?<name>[a-z]+)[=:](?<value>[^;]+);?");
+        t.transform(e);
+        Assert.assertEquals("key a not found", "1", e.get("a"));
+        Assert.assertEquals("key b not found", "2", e.get("b"));
+        Assert.assertEquals("key message not found", "c", e.get("message"));
+        
+    }
+    
+    @Test
+    public void test2() {
+        Event e = new Event();
+        e.put("message", "a=1;b:2");
+        VarExtractor t = new VarExtractor();
+        t.setField("message");
+        t.setParser("(?<name>[a-z]+)[=:](?<value>[^;]+);?");
+        t.transform(e);
+        Assert.assertEquals("key a not found", "1", e.get("a"));
+        Assert.assertEquals("key b found", "2", e.get("b"));
+        Assert.assertEquals("key message found", null, e.get("message"));
+        
+    }
+    
+    @Test
+    public void test3() {
+        Event e = new Event();
+        e.put("message", "a=1;b:2;c");
+        VarExtractor t = new VarExtractor();
+        t.setField("message");
+        t.transform(e);
+        Assert.assertEquals("key a not found", "1", e.get("a"));
+        Assert.assertEquals("key b not found", "2", e.get("b"));
+        Assert.assertEquals("key message not found", "c", e.get("message"));
+        
+    }
+}
