df4024b727fd0fc306e6d97ac6ae8bc625c6c82a
Fabrice Bacchella <fbacchella@spamcop.net>
2016-01-07 17:26:44 +0100

    Working grok processor

--- src/main/java/loghub/processors/Grok.java
+++ src/main/java/loghub/processors/Grok.java
@@ -1,29 +1,42 @@
 package loghub.processors;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URISyntaxException;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import loghub.Event;
+import loghub.Helpers;
 import loghub.Processor;
+import loghub.configuration.Properties;
 import oi.thekraken.grok.api.Match;
 import oi.thekraken.grok.api.exception.GrokException;
 
 public class Grok extends Processor {
+
+    private static final Logger logger = LogManager.getLogger();
+
     private final oi.thekraken.grok.api.Grok grok;
     private String field;
+    private String pattern;
 
     public Grok() {
-        try {
-            grok = oi.thekraken.grok.api.Grok.create("patterns/patterns");
-        } catch (GrokException e) {
-            throw new RuntimeException(e);
-        }
+        grok = new oi.thekraken.grok.api.Grok();
     }
 
     @Override
     public void process(Event event) {
         String line = (String) event.get(field);
-
         Match gm = grok.match(line);
         gm.captures();
-
+        gm.toMap().entrySet().stream()
+        .filter( i -> i.getValue() != null)
+        .forEach( i-> event.put(i.getKey(), i.getValue()));
     }
 
     public String getField() {
@@ -34,17 +47,35 @@ public class Grok extends Processor {
         this.field = field;
     }
 
-    public void setPattern(String pattern) throws GrokException {
-        grok.compile(pattern);
+    public void setPattern(String pattern) {
+        this.pattern = pattern;
     }
 
     public String getPattern() {
-        return grok.getOriginalGrokPattern();
+        return pattern;
     }
 
     @Override
     public String getName() {
         return "grok";
+    }
+
+    @Override
+    public boolean configure(Properties properties) {
+        Helpers.ThrowingConsumer<InputStream> grokloader = is -> grok.addPatternFromReader(new InputStreamReader(new BufferedInputStream(is)));
+        try {
+            Helpers.readRessources(properties.classloader, "patterns", grokloader);
+            grok.compile(pattern);
+        } catch (IOException | URISyntaxException e) {
+            logger.error("unable to load patterns: {}", e.getMessage());
+            logger.catching(Level.DEBUG, e);
+            return false;
+        } catch (GrokException e) {
+            logger.error("wrong pattern {}: {}", pattern, e.getMessage());
+            logger.catching(Level.DEBUG, e);
+            return false;
+        }
+        return super.configure(properties);
     }
 
 }
---
+++ src/test/java/loghub/processors/TestGrok.java
@@ -0,0 +1,33 @@
+package loghub.processors;
+
+import java.util.Collections;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import loghub.Event;
+import loghub.configuration.Properties;
+
+public class TestGrok {
+
+
+
+
+    @Test
+    public void TestLoadPatterns() {
+        Grok grok = new Grok();
+        grok.setField("message");
+        grok.setPattern("%{COMBINEDAPACHELOG}");
+        
+        Properties props = new Properties(Collections.emptyMap());
+        
+        Assert.assertTrue("Failed to configure grok", grok.configure(props));
+        
+        Event e = new Event();
+        e.put("message", "112.169.19.192 - - [06/Mar/2013:01:36:30 +0900] \"GET / HTTP/1.1\" 200 44346 \"-\" \"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.22 (KHTML, like Gecko) Chrome/25.0.1364.152 Safari/537.22\"");
+        grok.process(e);
+        
+        Assert.assertEquals("Didn't find the good user agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.22 (KHTML, like Gecko) Chrome/25.0.1364.152 Safari/537.22", e.get("agent"));
+    }
+}
+
