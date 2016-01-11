8d7d7dd96b6f787707024c3668b2231f192bccd7
Fabrice Bacchella <fbacchella@spamcop.net>
2016-01-11 14:26:38 +0100

    Fields processors can take glob to filter fields

--- src/main/java/loghub/processors/FieldsProcessor.java
+++ src/main/java/loghub/processors/FieldsProcessor.java
@@ -1,32 +1,48 @@
 package loghub.processors;
 
-import java.util.Arrays;
-import java.util.function.Consumer;
+import java.util.regex.Pattern;
 
 import loghub.Event;
+import loghub.Helpers;
 import loghub.Processor;
 import loghub.configuration.Beans;
 
 @Beans({"field", "fields"})
 public abstract class FieldsProcessor extends Processor {
     private String field = "message";
-    private String[] fields = new String[]{};
+    private String[] fields = new String[] {};
+    private Pattern[] patterns = new Pattern[]{};
 
     public abstract void processMessage(Event event, String field);
 
     @Override
     public void process(Event event) {
-        Consumer<String> process = i -> {if( event.containsKey(i) && event.get(i) != null) { processMessage(event, i);};};
-        process.accept(field);
-        Arrays.stream(fields).forEach(process);
+        if(patterns.length != 0) {
+            for(String f: event.keySet()) {
+                for(Pattern p: patterns) {
+                    if (p.matcher(f).matches()) {
+                        processMessage(event, f);
+                        // Processed, don't look for another matchin pattern
+                        break;
+                    }
+                }
+            }
+        } else {
+            if(event.containsKey(field) && event.get(field) != null) {
+                processMessage(event, field);
+            }
+        }
     }
 
-    public String[] getFields() {
+    public Object[] getFields() {
         return fields;
     }
 
-    public void setFields(String[] fields) {
-        this.fields = fields;
+    public void setFields(Object[] fields) {
+        this.patterns = new Pattern[fields.length];
+        for(int i = 0; i < fields.length ; i++) {
+            this.patterns[i] = Helpers.convertGlobToRegex(fields[i].toString());
+        }
     }
 
     public String getField() {
