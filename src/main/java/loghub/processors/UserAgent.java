6c4315905cbb2ec7c6c1479e8f7ee79f88bf1064
Fabrice Bacchella <fbacchella@spamcop.net>
2016-01-19 18:01:40 +0100

    Don't pull null informations in User Agent

--- src/main/java/loghub/processors/UserAgent.java
+++ src/main/java/loghub/processors/UserAgent.java
@@ -6,12 +6,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 import loghub.Event;
+import loghub.Helpers;
 import loghub.configuration.Properties;
 import ua_parser.CachingParser;
 import ua_parser.Client;
 import ua_parser.Parser;
 
 public class UserAgent extends FieldsProcessor {
+
+    private final static Helpers.TriConsumer<String, Object, Map<String, Object>> smartput = (a,b,c) -> { if (b != null) {c.put(a, b);};};
 
     private Parser uaParser;
     private int cacheSize = 1000;
@@ -21,24 +24,27 @@ public class UserAgent extends FieldsProcessor {
         String userAgent = event.get(field).toString();
         Client c = uaParser.parse(userAgent);
 
-
         Map<String, Object> ua = new HashMap<>(3);
-        ua.put("device", c.device.family);
+        smartput.accept("device", c.device.family, ua);
 
         Map<String, Object> os =  new HashMap<>(5);
-        os.put("family", c.os.family);
-        os.put("major", c.os.major);
-        os.put("minor", c.os.minor);
-        os.put("patch", c.os.patch);
-        os.put("patchMinor", c.os.patchMinor);
-        ua.put("os", os);
+        smartput.accept("family", c.os.family, os);
+        smartput.accept("major", c.os.major, os);
+        smartput.accept("minor", c.os.minor, os);
+        smartput.accept("patch", c.os.patch, os);
+        smartput.accept("patchMinor", c.os.patchMinor, os);
+        if(os.size() > 0) {
+            ua.put("os", os);
+        }
 
         Map<String, Object> agent =  new HashMap<>(4);
-        agent.put("family", c.userAgent.family);
-        agent.put("major", c.userAgent.major);
-        agent.put("minor", c.userAgent.minor);
-        agent.put("patch", c.userAgent.patch);
-        ua.put("userAgent", agent);
+        smartput.accept("family", c.userAgent.family, agent);
+        smartput.accept("major", c.userAgent.major, agent);
+        smartput.accept("minor", c.userAgent.minor, agent);
+        smartput.accept("patch", c.userAgent.patch, agent);
+        if(agent.size() > 0) {
+            ua.put("userAgent", agent);
+        }
 
         event.put(destination, ua);
     }
