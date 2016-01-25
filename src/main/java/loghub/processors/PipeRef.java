64ebed48cc1a56463e1a6323dd6ed1281798efa0
Fabrice Bacchella <fbacchella@spamcop.net>
2016-01-25 21:27:36 +0100

    PipeRef was not operational.

--- src/main/java/loghub/processors/PipeRef.java
+++ src/main/java/loghub/processors/PipeRef.java
@@ -1,32 +1,50 @@
 package loghub.processors;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import loghub.Event;
+import loghub.Pipeline;
 import loghub.Processor;
+import loghub.configuration.Properties;
 
 public class PipeRef extends Processor {
 
+    private static final Logger logger = LogManager.getLogger();
+
     private String pipeRef;
+    private Pipeline pipe;
+
+    @Override
+    public void process(Event event) {
+        try {
+            pipe.inQueue.put(event);
+            pipe.outQueue.take();
+        } catch (InterruptedException e) {
+        }
+    }
+
+    @Override
+    public String getName() {
+        return "piperef";
+    }
+
+    @Override
+    public boolean configure(Properties properties) {
+        pipe = properties.namedPipeLine.get(pipeRef);
+        if(pipe == null) {
+            logger.error("pipeline '{}' not found", pipeRef);
+            return false;
+        }
+        return super.configure(properties);
+    }
+
     public String getPipeRef() {
         return pipeRef;
     }
 
     public void setPipeRef(String pipeRef) {
         this.pipeRef = pipeRef;
-    }
-
-    public PipeRef() {
-        // TODO Auto-generated constructor stub
-    }
-
-    @Override
-    public void process(Event event) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public String getName() {
-        return "piperef";
     }
 
 }
--- src/test/java/loghub/processors/TestGrok.java
+++ src/test/java/loghub/processors/TestGrok.java
@@ -27,7 +27,24 @@ public class TestGrok {
         e.put("message", "112.169.19.192 - - [06/Mar/2013:01:36:30 +0900] \"GET / HTTP/1.1\" 200 44346 \"-\" \"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.22 (KHTML, like Gecko) Chrome/25.0.1364.152 Safari/537.22\"");
         grok.process(e);
 
-        Assert.assertEquals("Didn't find the good user agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.22 (KHTML, like Gecko) Chrome/25.0.1364.152 Safari/537.22", e.get("agent"));
+        Assert.assertEquals("Didn't find the good user agent", "\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.22 (KHTML, like Gecko) Chrome/25.0.1364.152 Safari/537.22\"", e.get("agent"));
+    }
+
+    @Test
+    public void TestLoadPatterns2() throws ProcessorException {
+        Grok grok = new Grok();
+        grok.setField("message");
+        grok.setPattern("(?:%{SYSLOG_LINE})");
+        
+        Properties props = new Properties(Collections.emptyMap());
+
+        Assert.assertTrue("Failed to configure grok", grok.configure(props));
+
+        EventWrapper e = new EventWrapper(new Event());
+        e.setProcessor(grok);
+        e.put("message", "<34>1 2016-01-25T12:28:00.164593+01:00 sys403 krb5kdc 4906 - -  closing down fd 14");
+        grok.process(e);
+
+        Assert.assertEquals("invalid syslog line matching", 8, e.size());
     }
 }
-
