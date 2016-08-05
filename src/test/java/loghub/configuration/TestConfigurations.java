9e3dbe3f7cab32382db8d41fe3b08c498c1816f7
Fabrice Bacchella <fabrice.bacchella@3ds.com>
2016-08-05 14:19:43 +0200

    Uses BlockingQueue.poll instead of BlockingQueue.take for some tests, hop to get more predictive results.

--- src/test/java/loghub/configuration/TestConfigurations.java
+++ src/test/java/loghub/configuration/TestConfigurations.java
@@ -1,6 +1,7 @@
 package loghub.configuration;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
@@ -26,18 +27,18 @@ public class TestConfigurations {
         LogUtils.setLevel(logger, Level.TRACE, "loghub.SmartContext", "loghub.Pipeline", "loghub.configuration","loghub.receivers.ZMQ", "loghub.Receiver", "loghub.processors.Forker", "loghub", "loghub.EventsProcessor");
     }
 
-    @Test(timeout=2000)
+    @Test
     public void testBuildPipeline() throws IOException, InterruptedException {
         Properties conf = Tools.loadConf("simple.conf");
         Event sent = Tools.getEvent();
         logger.debug("pipelines: " + conf.pipelines);
         logger.debug("namedPipeLine: " + conf.namedPipeLine);
         conf.mainQueue.add(sent);
-        Event received = conf.mainQueue.take();
+        Event received = conf.mainQueue.poll(1, TimeUnit.SECONDS);
         Assert.assertEquals("not expected event received", sent, received);
     }
 
-    @Test(timeout=2000)
+    @Test
     public void testBuildSubPipeline() throws IOException, InterruptedException {
         Properties conf = Tools.loadConf("simple.conf");
         Event sent = Tools.getEvent();
@@ -45,11 +46,11 @@ public class TestConfigurations {
         logger.debug("pipelines: " + conf.pipelines);
         logger.debug("namedPipeLine: " + conf.namedPipeLine);
         conf.mainQueue.add(sent);
-        Event received = conf.mainQueue.take();
+        Event received = conf.mainQueue.poll(1, TimeUnit.SECONDS);
         Assert.assertEquals("not expected event received", sent, received);
     }
 
-    @Test(timeout=2000) 
+    @Test
     public void testTwoPipe() throws InterruptedException {
         Properties conf = Tools.loadConf("twopipe.conf");
         Thread t = new EventsProcessor(conf.mainQueue, conf.outputQueues, conf.namedPipeLine, conf.maxSteps);
@@ -61,7 +62,7 @@ public class TestConfigurations {
         sent.inject(conf.namedPipeLine.get("pipeone"), conf.mainQueue);
 
         try {
-            Event re = conf.outputQueues.get("main").take();
+            Event re = conf.outputQueues.get("main").poll(1, TimeUnit.SECONDS);
             Assert.assertEquals("wrong event received", "1", re.get("message"));
         } finally {
             logger.debug("{} is at main pipeline {}", sent, sent.getCurrentPipeline());
@@ -69,7 +70,7 @@ public class TestConfigurations {
         }
     }
 
-    @Test(timeout=2000)
+    @Test
     public void testFork() throws InterruptedException, ProcessorException {
         Properties conf = Tools.loadConf("fork.conf");
         EventsProcessor ep = new EventsProcessor(conf.mainQueue, conf.outputQueues, conf.namedPipeLine, conf.maxSteps);
@@ -78,8 +79,8 @@ public class TestConfigurations {
         try {
             Event sent = Tools.getEvent();
             sent.inject(conf.namedPipeLine.get("main"), conf.mainQueue);
-            Event forked = conf.outputQueues.get("forked").take();
-            Event initial = conf.outputQueues.get("main").take();
+            Event forked = conf.outputQueues.get("forked").poll(1, TimeUnit.SECONDS);
+            Event initial = conf.outputQueues.get("main").poll(1, TimeUnit.SECONDS);
             Assert.assertEquals(1, forked.size());
             Assert.assertEquals(1, initial.size());
             Assert.assertEquals(2, forked.get("b"));
