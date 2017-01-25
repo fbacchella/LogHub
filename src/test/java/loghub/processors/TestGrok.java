861e8eeb6326f3e4fe1101e91af9ef05bdeef514
Fabrice Bacchella <fabrice.bacchella@3ds.com>
2017-01-25 11:23:22 +0100

    Forgot Grok test update.

--- src/test/java/loghub/processors/TestGrok.java
+++ src/test/java/loghub/processors/TestGrok.java
@@ -13,7 +13,7 @@ import loghub.configuration.Properties;
 public class TestGrok {
 
     @Test
-    public void TestLoadPatterns() throws ProcessorException {
+    public void TestLoadPatterns1() throws ProcessorException {
         Grok grok = new Grok();
         grok.setField("message");
         grok.setPattern("%{COMBINEDAPACHELOG}");
@@ -26,7 +26,7 @@ public class TestGrok {
         e.put("message", "112.169.19.192 - - [06/Mar/2013:01:36:30 +0900] \"GET / HTTP/1.1\" 200 44346 \"-\" \"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.22 (KHTML, like Gecko) Chrome/25.0.1364.152 Safari/537.22\"");
         e.process(grok);
 
-        Assert.assertEquals("Didn't find the good user agent", "\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.22 (KHTML, like Gecko) Chrome/25.0.1364.152 Safari/537.22\"", e.get("agent"));
+        Assert.assertEquals("Didn't find the good user agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.22 (KHTML, like Gecko) Chrome/25.0.1364.152 Safari/537.22", e.get("agent"));
     }
 
     @Test
