8048138c8d04fb8def547a341255f8bc0cb0d8a7
Fabrice Bacchella <fabrice.bacchella@3ds.com>
2017-01-31 22:38:05 +0100

    No more hack to filter grok named groups.

--- src/main/java/loghub/processors/Grok.java
+++ src/main/java/loghub/processors/Grok.java
@@ -43,10 +43,6 @@ public class Grok extends FieldsProcessor {
                 if (".".equals(e.getKey())) {
                     destinationField = field ;
                 }
-                //Dirty hack to filter non named regex
-                if (e.getKey().equals(e.getKey().toUpperCase()) && ! ".".equals(e.getKey())) {
-                    continue;
-                }
                 if (e.getValue() == null) {
                     continue;
                 }
@@ -88,7 +84,7 @@ public class Grok extends FieldsProcessor {
         Helpers.ThrowingConsumer<InputStream> grokloader = is -> grok.addPatternFromReader(new InputStreamReader(new BufferedInputStream(is)));
         try {
             Helpers.readRessources(properties.classloader, PATTERNSFOLDER, grokloader);
-            grok.compile(pattern);
+            grok.compile(pattern, true);
         } catch (IOException | URISyntaxException e) {
             logger.error("unable to load patterns: {}", e.getMessage());
             logger.catching(Level.DEBUG, e);
