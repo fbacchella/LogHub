c3c530d9dae543486df152fae1a1933cd5fae8d9
Fabrice Bacchella <fabrice.bacchella@3ds.com>
2017-09-27 10:04:22 +0200

    ElasticSearch manage templates.

--- src/main/java/loghub/senders/ElasticSearch.java
+++ src/main/java/loghub/senders/ElasticSearch.java
@@ -2,9 +2,14 @@ package loghub.senders;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.Reader;
+import java.io.StringReader;
+import java.io.UncheckedIOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.Charset;
+import java.nio.file.Paths;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Collections;
@@ -13,6 +18,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.TimeZone;
 import java.util.concurrent.BlockingQueue;
+import java.util.stream.Stream;
+
+import org.apache.logging.log4j.Level;
 
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonGenerator;
@@ -21,6 +29,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
 import loghub.Event;
+import loghub.configuration.Properties;
 
 public class ElasticSearch extends AbstractHttpSender {
 
@@ -35,19 +44,98 @@ public class ElasticSearch extends AbstractHttpSender {
     };
 
     private static final ThreadLocal<DateFormat> ISO8601 = ThreadLocal.withInitial( () -> new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ"));
-    private static final ThreadLocal<DateFormat> ES_INDEX = ThreadLocal.withInitial( () -> {
-        DateFormat df = new SimpleDateFormat("'logstash-'yyyy.MM.dd");
-        df.setTimeZone(TimeZone.getTimeZone("UTC"));
-        return df;
-    });
-
     // Beans
     private String type = "type";
+    private String indexformat = "'loghub-'yyyy.MM.dd";
+    private String templateName = "loghub";
+    private URL templatePath = getClass().getResource("/estemplate.json");
+
+    private ThreadLocal<DateFormat> esIndex;
 
     public ElasticSearch(BlockingQueue<Event> inQueue) {
         super(inQueue);
         setPort(9300);
     }
+
+    @Override
+    public boolean configure(Properties properties) {
+        boolean configured = false;
+        if (super.configure(properties)) {
+            esIndex = ThreadLocal.withInitial( () -> {
+                DateFormat df = new SimpleDateFormat(indexformat);
+                df.setTimeZone(TimeZone.getTimeZone("UTC"));
+                return df;
+            });
+            // Lets check for a template
+            String wantedtemplate;
+            try {
+                wantedtemplate = Stream.of(templatePath)
+                        .map( i -> {
+                            try {
+                                return i.openStream();
+                            } catch (IOException e) {
+                                throw new UncheckedIOException(e);
+                            }
+                        })
+                        .filter( i -> i != null)
+                        .map( i -> new InputStreamReader(i))
+                        .filter( i -> i != null)
+                        .map( i -> {
+                            try {
+                                return json.get().readTree(i).toString();
+                            } catch (IOException e) {
+                                throw new UncheckedIOException(e);
+                            }
+                        })
+                        .findFirst().orElseGet(() -> null);
+            } catch (UncheckedIOException e1) {
+                wantedtemplate = null;
+                logger.error("Can't load template definition: {}", e1.getMessage());
+                logger.catching(Level.DEBUG, e1);
+            }
+            System.out.println(templatePath);
+            if (wantedtemplate != null) {
+                for (String destination: getDestinations()) {
+                    try {
+                        URL newEndPoint = new URL(destination);
+                        newEndPoint = new URL(newEndPoint.getProtocol(), newEndPoint.getHost(), newEndPoint.getPort(), newEndPoint.getFile() + "/_template/" + templateName);
+                        HttpRequest gettemplate = new HttpRequest();
+                        gettemplate.setUrl(newEndPoint);
+                        HttpResponse response = doRequest(gettemplate);
+                        try {
+                            boolean needsrefresh = response.getStatus() == 404;
+                            if (!needsrefresh ) {
+                                String currenttemplate = json.get().readTree(response.getContentReader()).toString();
+                                needsrefresh = ! currenttemplate.equals(wantedtemplate);
+                            }
+                            if (needsrefresh) {
+                                HttpRequest puttemplate = new HttpRequest();
+                                puttemplate.setVerb("PUT");
+                                puttemplate.setUrl(newEndPoint);
+                                puttemplate.setTypeAndContent("application/json", Charset.forName("UTF-8"), new StringReader(wantedtemplate));
+                                response = doRequest(puttemplate);
+                                int status = response.getStatus();
+                                if ((status - status % 100) != 200 && "application/json".equals(response.getMimeType())) {
+                                    System.out.println(json.get().readTree(response.getContentReader()).toString());
+                                } else {
+                                    configured = true;
+                                    break;
+                                }
+                                break;
+                            }
+                        } catch (IOException e) {
+                        }
+                    } catch (MalformedURLException e) {
+                    }
+                }
+            } else {
+                configured = true;
+            }
+        }
+        return configured;
+    }
+
+
 
     protected void putContent(List<Event> documents, ByteArrayOutputStream buffer) {
         Map<String, String> settings = new HashMap<>(2);
@@ -57,7 +145,7 @@ public class ElasticSearch extends AbstractHttpSender {
             Map<String, Object> esjson = new HashMap<>(e.size());
             esjson.putAll(e);
             esjson.put("@timestamp", ISO8601.get().format(e.getTimestamp()));
-            esjson.put("__index", ES_INDEX.get().format(e.getTimestamp()));
+            esjson.put("__index", esIndex.get().format(e.getTimestamp()));
             return esjson;
         })
         .filter( i -> i.containsKey(type))
@@ -73,8 +161,7 @@ public class ElasticSearch extends AbstractHttpSender {
             } catch (IOException e1) {
                 // Unreachable exception, no IO exception on ByteArrayOutputStream
             }
-        }
-                );
+        });
 
         try {
             buffer.flush();
@@ -83,13 +170,13 @@ public class ElasticSearch extends AbstractHttpSender {
     }
 
     @Override
-    protected <T> T scanContent(Response resp) {
+    protected <T> T scanContent(HttpResponse resp) {
         try (Reader contentReader = getSmartContentReader(resp, ContentType.APPLICATION_JSON)) {
             @SuppressWarnings("unchecked")
             T o = (T) json.get().readValue(contentReader, Object.class);
             return o;
         } catch (IllegalStateException e) {
-            logger.error("bad response content from {}: {}", resp.getHost(), resp.getContentType());
+            logger.error("bad response content from {}: {}", resp.getHost(), resp.getMimeType());
             resp.close();
             return null;
         } catch (UnsupportedOperationException | IOException e) {
@@ -141,6 +228,49 @@ public class ElasticSearch extends AbstractHttpSender {
     @Override
     protected String getPath(URL tryUrl) {
         return tryUrl.getPath() + "/_bulk";
+    }
+
+    /**
+     * @return the indexformat
+     */
+    public String getIndexformat() {
+        return indexformat;
+    }
+
+    /**
+     * @param indexformat the indexformat to set
+     */
+    public void setIndexformat(String indexformat) {
+        this.indexformat = indexformat;
+    }
+
+    /**
+     * @return the templateName
+     */
+    public String getTemplateName() {
+        return templateName;
+    }
+
+    /**
+     * @param templateName the templateName to set
+     */
+    public void setTemplateName(String templateName) {
+        this.templateName = templateName;
+    }
+
+    /**
+     * @return the templatePath
+     */
+    public String getTemplatePath() {
+        return templatePath.getFile();
+    }
+
+    /**
+     * @param templatePath the templatePath to set
+     * @throws MalformedURLException 
+     */
+    public void setTemplatePath(String templatePath) throws MalformedURLException {
+        this.templatePath = Paths.get(templatePath).toUri().toURL();
     }
 
 }
