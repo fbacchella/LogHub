73c1e36e79a3967cc25749e58f2a6fa6ca4eef2a
Fabrice Bacchella <fabrice.bacchella@3ds.com>
2017-10-10 14:29:00 +0200

    Still working on it.

--- src/main/java/loghub/DashboardHttpServer.java
+++ src/main/java/loghub/DashboardHttpServer.java
@@ -4,6 +4,7 @@ import io.netty.channel.ChannelPipeline;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.FullHttpRequest;
 import loghub.netty.http.AbstractHttpServer;
+import loghub.netty.http.DateparsingCheck;
 import loghub.netty.http.JmxProxy;
 import loghub.netty.http.NotFound;
 import loghub.netty.http.ResourceFiles;
@@ -14,12 +15,14 @@ public class DashboardHttpServer extends AbstractHttpServer {
     private final SimpleChannelInboundHandler<FullHttpRequest> ROOTREDIRECT = new RootRedirect();
     private final SimpleChannelInboundHandler<FullHttpRequest> NOTFOUND = new NotFound();
     private final SimpleChannelInboundHandler<FullHttpRequest> JMXPROXY = new JmxProxy();
+    private final SimpleChannelInboundHandler<FullHttpRequest> DATAPARSINGCHECK = new DateparsingCheck();
 
     @Override
     public void addModelHandlers(ChannelPipeline p) {
         p.addLast(ROOTREDIRECT);
         p.addLast(new ResourceFiles());
         p.addLast(JMXPROXY);
+        p.addLast(DATAPARSINGCHECK);
         p.addLast(NOTFOUND);
     }
 
