e53ccd864913e5e011b3a4747e3524b2e5df2f18
Fabrice Bacchella <fabrice.bacchella@3ds.com>
2017-10-19 09:59:05 +0200

    Forgotten file for new netflow.

--- src/main/java/loghub/decoders/netflow/NetflowDecoder.java
+++ src/main/java/loghub/decoders/netflow/NetflowDecoder.java
@@ -3,18 +3,19 @@ package loghub.decoders.netflow;
 import java.util.Map;
 
 import io.netty.buffer.ByteBuf;
+import loghub.ConnectionContext;
 import loghub.Decoder;
 
 public class NetflowDecoder extends Decoder {
 
     @Override
-    public Map<String, Object> decode(byte[] msg, int offset, int length) throws DecodeException {
+    public Map<String, Object> decode(ConnectionContext ctx, byte[] msg, int offset, int length) throws DecodeException {
         // TODO Auto-generated method stub
         return null;
     }
 
     @Override
-    public Map<String, Object> decode(ByteBuf bbuf) throws DecodeException {
+    public Map<String, Object> decode(ConnectionContext ctx, ByteBuf bbuf) throws DecodeException {
         // TODO Auto-generated method stub
         return null;
     }
