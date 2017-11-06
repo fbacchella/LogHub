4d842eb5bfb76900b263a8fcf3bf1fd11721fd34
Fabrice Bacchella <fabrice.bacchella@3ds.com>
2017-11-06 23:15:41 +0100

    PrintBinary renamed as ScanBinary. Can be used to identify set bits or split a value as subfields.

---
+++ src/main/java/loghub/processors/ScanBinary.java
@@ -0,0 +1,120 @@
+package loghub.processors;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.DoubleAccumulator;
+import java.util.concurrent.atomic.DoubleAdder;
+import java.util.stream.IntStream;
+
+import loghub.Event;
+import loghub.ProcessorException;
+import loghub.configuration.Properties;
+
+public class ScanBinary extends FieldsProcessor {
+
+    private Object[] bitsNames = new Object[0];
+    private int[] fieldsLength = null;
+
+    @Override
+    public boolean configure(Properties properties) {
+        if (bitsNames == null || bitsNames.length == 0) {
+            logger.error("Missing mandatory attribute bitsNames");
+            return false;
+        }
+        return super.configure(properties);
+    }
+
+    @Override
+    public boolean processMessage(Event event, String field, String destination) throws ProcessorException {
+        Object value = event.get(field);
+        long nvalue = 0;
+        if (value == null) {
+            event.put(destination, null);
+            return true;
+        } else if (value instanceof Number) {
+            if (value instanceof BigDecimal || value instanceof Double || value instanceof DoubleAccumulator || value instanceof DoubleAdder || value instanceof Float) {
+                // not bit field for floating point values
+                return false;
+            }
+            else {
+                nvalue = ((Number)value).longValue();
+            }
+        } else if (value instanceof String) {
+            try {
+                nvalue = Long.decode((String)value);
+            } catch (NumberFormatException e) {
+                throw event.buildException("Can't parse as number " + value.toString(), e);
+            }
+        }
+        if (nvalue < 0) {
+            // not bit field for negative values
+            return false;
+        }
+        if (fieldsLength == null) {
+            List<String> parsed = new ArrayList<>(bitsNames.length);
+            for (int i = 0 ; nvalue > 0 && i < bitsNames.length ; nvalue = nvalue >> 1 , i++) {
+                if ((nvalue & 1) == 1) {
+                    parsed.add(bitsNames[i].toString());
+                }
+            }
+            event.put(destination, parsed.toArray(new String[parsed.size()]));
+        } else {
+            Map<String, Number> values = new HashMap<>(fieldsLength.length);
+            for (int i = 0 ; i < fieldsLength.length  ; i++) {
+                int mask = (1 << fieldsLength[i]) - 1;
+                values.put(bitsNames[i].toString(), nvalue & mask);
+                nvalue = nvalue >> fieldsLength[i];
+            }
+            event.put(destination, values);
+        }
+        return true;
+    }
+
+    /**
+     * @return the matching
+     */
+    public Object[] getBitsNames() {
+        return bitsNames;
+    }
+
+    /**
+     * @param matching the matching to set
+     */
+    public void setBitsNames(Object[] matching) {
+        this.bitsNames = matching;
+    }
+
+    /**
+     * @return the fieldsLength, can be null
+     */
+    public Object[] getFieldsLength() {
+        if(fieldsLength != null) {
+            Object[] returned = new Object[fieldsLength.length];
+            IntStream.range(0, fieldsLength.length).forEach(i -> {
+                returned[i] = fieldsLength[i];
+            });
+            return returned;
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * @param fieldsLength the fieldsLength to set
+     */
+    public void setFieldsLength(Object[] lengths) {
+        this.fieldsLength = new int[lengths.length];
+        IntStream.range(0, lengths.length)
+        .forEach(i -> {
+            try {
+                this.fieldsLength[i] = ((Number) lengths[i]).intValue();
+            } catch (NumberFormatException e) {
+                this.fieldsLength[i] = 0;
+            }
+        });
+    }
+
+}
--- src/test/java/loghub/processors/TestPrintBinary.java
+++ src/test/java/loghub/processors/TestPrintBinary.java
@@ -1,32 +1,38 @@
 package loghub.processors;
 
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Map;
 
 import org.junit.Assert;
 import org.junit.Test;
 
 import loghub.ConnectionContext;
 import loghub.Event;
+import loghub.Pipeline;
 import loghub.ProcessorException;
+import loghub.Tools;
+import loghub.configuration.ConfigException;
 import loghub.configuration.Properties;
 
 public class TestPrintBinary {
 
     @Test
     public void simpleTestWithNames() throws ProcessorException {
-        PrintBinary fs = new PrintBinary();
+        ScanBinary fs = new ScanBinary();
         fs.setBitsNames(new String[] {"PF_PROT", "PF_WRITE", "PF_USER", "PF_RSVD", "PF_INSTR"});
         fs.configure(new Properties(Collections.emptyMap()));
 
         Event e = Event.emptyEvent(ConnectionContext.EMPTY);
         e.put("binary", "13");
-        fs.processMessage(e, "binary", "value");
+        Assert.assertTrue(fs.processMessage(e, "binary", "value"));
         Assert.assertArrayEquals("Bad decoding of bitfield", new String[] {"PF_PROT", "PF_USER", "PF_RSVD"}, (String[])e.get("value"));
     }
 
     @Test
     public void simpleTestWithVariableLengthNames() throws ProcessorException {
-        PrintBinary fs = new PrintBinary();
+        ScanBinary fs = new ScanBinary();
         fs.setBitsNames(new String[] {"Busmaster I/O read", "Memory test", "Modem firmware", "Modem UART", "Serial port UART",
                 "Keyboard interface", "Battery interface", "NVRAM interface", "NVRAM write / read / verify",
                 "Video", "PCMCIA", "NIC", "Mouse interface", "CPLD", "SRAM", "EEPROM", "I2C"});
@@ -34,18 +40,49 @@ public class TestPrintBinary {
 
         Event e = Event.emptyEvent(ConnectionContext.EMPTY);
         e.put("binary", "8192");
-        fs.processMessage(e, "binary", "value");
+        Assert.assertTrue(fs.processMessage(e, "binary", "value"));
         Assert.assertArrayEquals("Bad decoding of bitfield", new String[] {"CPLD"}, (String[])e.get("value"));
+        System.out.println(Arrays.toString((Object[])e.get("value")));
+    }
+
+    @Test
+    public void simpleTestWithVariableLengthName2s() throws ProcessorException {
+        ScanBinary fs = new ScanBinary();
+        fs.setBitsNames(new String[] {"a", "b", "c"});
+        fs.setFieldsLength(new Integer[] {3, 2, 1});
+        fs.configure(new Properties(Collections.emptyMap()));
+
+        Event e = Event.emptyEvent(ConnectionContext.EMPTY);
+        e.put("binary", 0b110101);
+        Assert.assertTrue(fs.processMessage(e, "binary", "value"));
+        @SuppressWarnings("unchecked")
+        Map<String, Number> value = (Map<String, Number>) e.get("value");
+        Assert.assertEquals(0b101, value.get("a").intValue());
+        Assert.assertEquals(0b10, value.get("b").intValue());
+        Assert.assertEquals(0b1, value.get("c").intValue());
     }
 
     @Test
     public void simpleTestNoName() throws ProcessorException {
-        PrintBinary fs = new PrintBinary();
-        fs.configure(new Properties(Collections.emptyMap()));
-
-        Event e = Event.emptyEvent(ConnectionContext.EMPTY);
-        e.put("binary", "14");
-        fs.processMessage(e, "binary", "value");
-        Assert.assertEquals("Bad decoding of bitfield", "0b1110", e.get("value"));
+        ScanBinary fs = new ScanBinary();
+        Assert.assertFalse(fs.configure(new Properties(Collections.emptyMap())));
     }
+
+    @Test
+    public void testConfigFile() throws ProcessorException, InterruptedException, ConfigException, IOException {
+        Properties conf = Tools.loadConf("scanbinary.conf");
+        for (Pipeline pipe: conf.pipelines) {
+            Assert.assertTrue("configuration failed", pipe.configure(conf));
+        }
+        Event sent = Tools.getEvent();
+        sent.put("binary", 0b110101);
+
+        Tools.runProcessing(sent, conf.namedPipeLine.get("main"), conf);
+        @SuppressWarnings("unchecked")
+        Map<String, Number> value = (Map<String, Number>) sent.get("binary");
+        Assert.assertEquals(0b101, value.get("a").intValue());
+        Assert.assertEquals(0b10, value.get("b").intValue());
+        Assert.assertEquals(0b1, value.get("c").intValue());
+    }
+
 }
