9b7680f5061802c265dc1b3206d5c402bc4f3fcd
Fabrice Bacchella <fabrice.bacchella@3ds.com>
2017-11-29 22:08:07 +0100

    Failed to generate javadoc because of logstash.beats.IMessageListener.

--- src/main/java/org/logstash/beats/IMessageListener.java
+++ src/main/java/org/logstash/beats/IMessageListener.java
@@ -12,15 +12,15 @@ public interface IMessageListener {
      * This is triggered on every new message parsed by the beats handler
      * and should be executed in the ruby world.
      *
-     * @param ctx
-     * @param message
+     * @param ctx context
+     * @param message message
      */
     public void onNewMessage(ChannelHandlerContext ctx, Message message);
 
     /**
      * Triggered when a new client connect to the input, this is used to link a connection
      * to a codec in the ruby world.
-     * @param ctx
+     * @param ctx context
      */
     public void onNewConnection(ChannelHandlerContext ctx);
 
@@ -28,7 +28,7 @@ public interface IMessageListener {
      * Triggered when a connection is close on the remote end and we need to flush buffered
      * events to the queue.
      *
-     * @param ctx
+     * @param ctx context
      */
     public void onConnectionClose(ChannelHandlerContext ctx);
 
@@ -36,16 +36,16 @@ public interface IMessageListener {
      * Called went something bad occur in the pipeline, allow to clear buffered codec went
      * somethign goes wrong.
      *
-     * @param ctx
-     * @param cause
+     * @param ctx context
+     * @param cause cause
      */
     public void onException(ChannelHandlerContext ctx, Throwable cause);
 
     /**
      * Called when a error occur in the channel initialize, usually ssl handshake error.
      *
-     * @param ctx
-     * @param cause
+     * @param ctx context
+     * @param cause cause
      */
     public void onChannelInitializeException(ChannelHandlerContext ctx, Throwable cause);
 }
