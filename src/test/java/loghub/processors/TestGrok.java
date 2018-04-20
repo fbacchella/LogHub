32d9904c58752a9aa999ffc255cdc951c387486f
Fabrice Bacchella <fabrice.bacchella@orange.fr>
2018-04-20 15:48:46 +0200

    https://github.com/thekrakken/java-grok/issues/64 is closed. New maven dependency.

--- src/test/java/loghub/processors/TestGrok.java
+++ src/test/java/loghub/processors/TestGrok.java
@@ -1,15 +1,10 @@
 package loghub.processors;
 
 import java.util.Collections;
-import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.junit.Assert;
 import org.junit.Test;
 
-import io.krakens.grok.api.GrokCompiler;
-import io.krakens.grok.api.Match;
 import loghub.Event;
 import loghub.ProcessorException;
 import loghub.Tools;
@@ -121,29 +116,6 @@ public class TestGrok {
         Event e = Tools.getEvent();
         e.put("host", "www.yahoo.com");
         Assert.assertFalse(grok.processMessage(e, "host", "host"));
-    }
-
-    // Will fails when issue https://github.com/thekrakken/java-grok/issues/64 is corrected
-    @Test
-    public void TestLoadPatterns8() {
-        String pattern = "(?<message>client id): (?<clientid>.*)";
-        String input = "client id: \"name\" \"Mac OS X Mail\" \"version\" \"10.2 (3259)\" \"os\" \"Mac OS X\" \"os-version\" \"10.12.3 (16D32)\" \"vendor\" \"Apple Inc.\"";
-
-        // Validate the search is good
-        Pattern p = Pattern.compile(pattern);
-        Matcher m = p.matcher(input);
-        if (m.matches()) {
-            Assert.assertEquals("\"name\" \"Mac OS X Mail\" \"version\" \"10.2 (3259)\" \"os\" \"Mac OS X\" \"os-version\" \"10.12.3 (16D32)\" \"vendor\" \"Apple Inc.\"", m.group("clientid"));
-        }
-
-        GrokCompiler grokCompiler = GrokCompiler.newInstance();
-        grokCompiler.registerDefaultPatterns();
-
-        io.krakens.grok.api.Grok grok = grokCompiler.compile(pattern, true);
-
-        Match gm = grok.match(input);
-        Map<String, Object> captures = gm.capture();
-        Assert.assertNotEquals(captures.get("clientid"), gm.getMatch().group("clientid"));
     }
 
 }
