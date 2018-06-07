c50da5556daf4cbe51d88c2e8d67bd8402dee2f7
Fabrice Bacchella <fabrice.bacchella@3ds.com>
2018-06-07 07:02:58 +0200

    Forgot a class needed for tests.

---
+++ src/test/java/loghub/senders/InMemorySender.java
@@ -0,0 +1,28 @@
+package loghub.senders;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import loghub.Event;
+import loghub.Sender;
+
+public class InMemorySender extends Sender {
+    private final List<Event> received = new ArrayList<>();
+    
+    @Override
+    public boolean send(Event e) {
+        received.add(e);
+        return true;
+    }
+
+    @Override
+    public String getSenderName() {
+        return null;
+    }
+    
+    public List<Event> getSendedEvents() {
+        return Collections.unmodifiableList(received);
+    }
+
+}
