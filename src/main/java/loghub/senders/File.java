f33eb0d6d6bd31958917760d0980b078997923fc
Fabrice Bacchella <fabrice.bacchella@3ds.com>
2018-06-16 12:38:34 +0200

    Improved File sender.

--- src/main/java/loghub/senders/File.java
+++ src/main/java/loghub/senders/File.java
@@ -1,60 +1,77 @@
 package loghub.senders;
 
 import java.io.IOException;
-import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousCloseException;
 import java.nio.channels.AsynchronousFileChannel;
+import java.nio.channels.ClosedChannelException;
 import java.nio.channels.CompletionHandler;
-import java.nio.channels.FileLock;
 import java.nio.channels.NonWritableChannelException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.util.concurrent.ExecutionException;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.apache.logging.log4j.Level;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import loghub.Event;
+import loghub.Helpers;
 import loghub.Sender;
 import loghub.configuration.Properties;
 
+@AsyncSender
 public class File extends Sender {
 
-    private final CompletionHandler<Integer, Object> handler = new CompletionHandler<Integer, Object>() {
+    private final CompletionHandler<Integer, Event> handler = new CompletionHandler<Integer, Event>() {
 
         @Override
-        public void completed(Integer result, Object attachment) {
+        public void completed(Integer result, Event event) {
+            event.getConnectionContext().acknowledge();
+            CompletableFuture<Boolean> done = CompletableFuture.completedFuture(true);
+            File.this.processStatus(event, done);
         }
 
         @Override
-        public void failed(Throwable exc, Object attachment) {
-            String message = exc.getMessage();
-            if (message == null) {
-                message = exc.getClass().getSimpleName();
+        public void failed(Throwable ex, Event event) {
+            if (ex instanceof AsynchronousCloseException || ex instanceof ClosedChannelException) {
+                CompletableFuture<Boolean> failed = new CompletableFuture<>();
+                failed.complete(false);
+                File.this.processStatus(event, failed);
+            } else {
+                CompletableFuture<Boolean> failed = new CompletableFuture<>();
+                failed.completeExceptionally(ex);
+                File.this.processStatus(event, failed);
             }
-            logger.error("error writing event to {}",fileName, message);
         }
 
     };
+
     private String fileName;
+    private String separator = "";
+    private byte[] separatorBytes = new byte[] {};
+    private boolean truncate = false;
 
     private AsynchronousFileChannel destination;
     private AtomicLong position;
-
-    public File() {
-    }
 
     @Override
     public boolean configure(Properties properties) {
         try {
             destination = AsynchronousFileChannel.open(Paths.get(fileName), StandardOpenOption.WRITE, StandardOpenOption.CREATE);
-            try(FileLock l = destination.lock().get()) {
-                position = new AtomicLong(l.position());
+            // Failed to used StandardOpenOption.(APPEND vs CREATE), so truncate if need after creation
+            if (truncate) {
+                destination.truncate(0);
             }
-        } catch (InterruptedException | ExecutionException | IOException | UnsupportedOperationException e) {
-            String message = e.getMessage();
-            if (message == null) {
-                message = e.getClass().getSimpleName();
-            }
-            logger.error("error openening output file {}: {}", fileName, message);
+            position = new AtomicLong(destination.size());
+        } catch (IOException | UnsupportedOperationException e) {
+            logger.error("error openening output file {}: {}", fileName, Helpers.resolveThrowableException(e));
+            logger.catching(Level.DEBUG, e);
             return false;
+        }
+        if (separator.length() > 0) {
+            separatorBytes = separator.getBytes(StandardCharsets.UTF_8);
         }
         return super.configure(properties);
     }
@@ -63,13 +80,46 @@ public class File extends Sender {
     public boolean send(Event event) {
         try {
             byte[] msg = getEncoder().encode(event);
-            long writepose = position.getAndAdd(msg.length);
-            destination.write(ByteBuffer.wrap(msg), writepose, null, handler);
+            ByteBuf buffer = Unpooled.buffer(msg.length
+                                             + separatorBytes.length);
+            buffer.writeBytes(msg);
+            buffer.writeBytes(separatorBytes);
+            long writepose = position.getAndAdd(msg.length
+                                                + separatorBytes.length);
+            destination.write(buffer.nioBuffer(), writepose, event,
+                              handler);
             return true;
         } catch (IllegalArgumentException | NonWritableChannelException e) {
-            logger.error("error writing event to {}: {}", fileName, e.getMessage());
+            logger.error("error writing event to {}: {}", fileName,
+                         Helpers.resolveThrowableException(e));
+            logger.catching(Level.DEBUG, e);
             return false;
+        } 
+    }
+
+    /**
+     * Used by unit test, don't use it
+     * @throws IOException
+     */
+    void close() throws IOException {
+        destination.close();
+    }
+
+    @Override
+    public void stopSending() {
+        if (destination.isOpen()) {
+            try {
+                // The lock will not be released, as you don't release a closed file
+                destination.lock();
+                destination.force(true);
+                destination.close();
+            } catch (IOException e) {
+                logger.error("Failed to close {}: {}", fileName,
+                             Helpers.resolveThrowableException(e));
+                logger.catching(Level.DEBUG, e);
+            } 
         }
+        super.stopSending();
     }
 
     @Override
@@ -89,6 +139,28 @@ public class File extends Sender {
      */
     public void setFileName(String fileName) {
         this.fileName = fileName;
+    }
+
+    /**
+     * @return the separator
+     */
+    public String getSeparator() {
+        return separator;
+    }
+
+    /**
+     * @param separator the separator to set
+     */
+    public void setSeparator(String separator) {
+        this.separator = separator;
+    }
+
+    public boolean isTruncate() {
+        return truncate;
+    }
+
+    public void setTruncate(boolean truncate) {
+        this.truncate = truncate;
     }
 
 }
---
+++ src/test/java/loghub/senders/TestFile.java
@@ -0,0 +1,152 @@
+package loghub.senders;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.Semaphore;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import loghub.ConnectionContext;
+import loghub.Event;
+import loghub.LogUtils;
+import loghub.Stats;
+import loghub.Tools;
+import loghub.configuration.Properties;
+import loghub.encoders.StringField;
+
+public class TestFile {
+
+    /**
+     * Allows to check that asynchronous acknowledge is indeed being called
+     * @author Fabrice Bacchella
+     *
+     */
+    private class BlockingConnectionContext extends ConnectionContext<Semaphore> {
+        Semaphore lock = new Semaphore(1);
+
+        BlockingConnectionContext() {
+            lock.drainPermits();
+        }
+
+        @Override
+        public Semaphore getLocalAddress() {
+            return lock;
+        }
+
+        @Override
+        public Semaphore getRemoteAddress() {
+            return lock;
+        }
+        @Override
+        public void acknowledge() {
+            lock.release();
+        }
+
+    }
+
+    private static Logger logger = LogManager.getLogger();
+
+    @BeforeClass
+    static public void configure() throws IOException {
+        Tools.configure();
+        logger = LogManager.getLogger();
+        LogUtils.setLevel(logger, Level.TRACE, "loghub.senders.File", "loghub.encoders.StringField");
+    }
+
+    @Rule
+    public TemporaryFolder folder= new TemporaryFolder();
+
+    private String outFile;
+    private final ArrayBlockingQueue<Event> queue = new ArrayBlockingQueue<>(10);
+
+    @Before
+    public void reset() {
+        Stats.reset();
+    }
+
+    private File send(java.util.function.Consumer<File> prepare, long expectedSize, boolean close) throws IOException, InterruptedException {
+        outFile = Paths.get(folder.getRoot().getCanonicalPath(), "file1").toAbsolutePath().toString();
+
+        File fsend = new File();
+        fsend.setFileName(outFile);
+        fsend.setInQueue(queue);
+        StringField sf = new StringField();
+        sf.setFormat("${message%s}");
+        fsend.setEncoder(sf);
+
+        prepare.accept(fsend);
+
+        Assert.assertTrue(fsend.configure(new Properties(Collections.emptyMap())));
+        fsend.start();
+
+        Event ev = Event.emptyEvent(new BlockingConnectionContext());
+        ev.put("message", 1);
+        queue.add(ev);
+        ConnectionContext<Semaphore> ctxt = ev.getConnectionContext();
+        ctxt.getLocalAddress().acquire();
+
+        if (close) {
+            fsend.stopSending();
+            Assert.assertEquals(expectedSize, new java.io.File(outFile).length());
+        }
+        return fsend;
+    }
+
+    @Test(timeout=500)
+    public void testOk() throws IOException, InterruptedException {
+        send(i -> i.setTruncate(true), 1, true);
+        send(i -> i.setTruncate(true), 1, true);
+        Assert.assertEquals(2L, Stats.sent.get());
+    }
+
+    @Test(timeout=500)
+    public void testOkAppend() throws IOException, InterruptedException {
+        send(i -> i.setTruncate(false), 1, true);
+        send(i -> i.setTruncate(false), 2, true);
+        Assert.assertEquals(2L, Stats.sent.get());
+    }
+
+    @Test(timeout=500)
+    public void testOkSeparator() throws IOException, InterruptedException {
+        send(i -> {i.setTruncate(true) ; i.setSeparator("\n");}, 2, true);
+        send(i -> i.setTruncate(true), 1, true);
+        Assert.assertEquals(2L, Stats.sent.get());
+    }
+
+    @Test
+    public void testBrokenFormatter() throws IOException, InterruptedException {
+        outFile = Paths.get(folder.getRoot().getCanonicalPath(), "file1").toAbsolutePath().toString();
+        File fsend = new File();
+        fsend.setFileName(outFile);
+        fsend.setInQueue(queue);
+        StringField sf = new StringField();
+        sf.setFormat("${");
+        fsend.setEncoder(sf);
+        Assert.assertFalse(fsend.configure(new Properties(Collections.emptyMap())));
+    }
+
+    @Test()
+    public void testFailing() throws IOException, InterruptedException {
+        File fsend = send(i -> {}, -1, false);
+        new java.io.File(fsend.getFileName()).setWritable(false, false);
+        Files.setPosixFilePermissions(Paths.get(fsend.getFileName()), Collections.emptySet());
+        Event ev = Tools.getEvent();
+        ev.put("message", 2);
+        fsend.close();
+        fsend.send(ev);
+        Thread.sleep(100);
+        Assert.assertEquals(1L, Stats.failed.get());
+    }
+
+}
