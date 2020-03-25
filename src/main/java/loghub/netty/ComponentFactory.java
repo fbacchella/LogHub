44a22731a93a3d7e243a33497901a53c80b10a45
Fabrice Bacchella <fabrice.bacchella@orange.fr>
2020-03-25 20:28:52 +0100

    Slightly cleaning of Netty ComponentFactory.

--- src/main/java/loghub/netty/ComponentFactory.java
+++ src/main/java/loghub/netty/ComponentFactory.java
@@ -1,5 +1,6 @@
 package loghub.netty;
 
+import java.io.IOException;
 import java.net.SocketAddress;
 import java.util.concurrent.ThreadFactory;
 
@@ -11,7 +12,7 @@ import org.apache.logging.log4j.Logger;
 import io.netty.bootstrap.AbstractBootstrap;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelFactory;
-import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandler.Sharable;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.EventLoopGroup;
@@ -21,6 +22,40 @@ import loghub.Helpers;
 import loghub.netty.servers.AbstractNettyServer;
 
 public abstract class ComponentFactory<BS extends AbstractBootstrap<BS,BSC>, BSC extends Channel, SA extends SocketAddress> {
+
+    @Sharable
+    private static class ErrorHandler extends SimpleChannelInboundHandler<Object> {
+        private final Logger logger;
+        private ErrorHandler(Logger logger) {
+            this.logger = logger;
+        }
+
+        @Override
+        protected void channelRead0(ChannelHandlerContext ctx,
+                                    Object msg)
+                                                    throws Exception {
+            logger.warn("Not processed message {}", msg);
+        }
+        @Override
+        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause)
+                        throws Exception {
+            if (cause.getCause() instanceof NotSslRecordException) {
+                logger.warn("Not a SSL connexion from {} on SSL listen", ctx.channel().remoteAddress());
+            } else if (cause.getCause() instanceof SSLHandshakeException) {
+                logger.warn("Failed SSL handshake from {}: {}", ctx.channel().remoteAddress(), Helpers.resolveThrowableException(cause));
+                logger.catching(Level.DEBUG, cause);
+            } else if (cause instanceof IOException) {
+                logger.warn("IO Exception from {}: {}", ctx.channel().remoteAddress(), Helpers.resolveThrowableException(cause));
+                logger.catching(Level.DEBUG, cause);
+            } else {
+                logger.warn("Not handled exception {} from {}", Helpers.resolveThrowableException(cause), ctx.channel().remoteAddress());
+                logger.throwing(Level.WARN, cause);
+            }
+            ctx.close().await();
+            logger.warn("channel closed");
+        }
+    }
+
     public abstract EventLoopGroup getEventLoopGroup(int threads, ThreadFactory threadFactory);
     public abstract ChannelFactory<BSC> getInstance();
     public abstract BS getBootStrap();
@@ -29,28 +64,7 @@ public abstract class ComponentFactory<BS extends AbstractBootstrap<BS,BSC>, BSC
     public abstract void addChildhandlers(ChannelConsumer<BS, BSC> source, AbstractNettyServer<?, BS, BSC, ?, SA, ?, ?> server, Logger logger);
     public void addHandlers(ChannelConsumer<BS, BSC> source, AbstractNettyServer<?, BS, BSC, ?, SA, ?, ?> server, Logger logger) { };
     protected void addErrorHandler(ChannelPipeline p, Logger logger) {
-        ChannelHandler h = new SimpleChannelInboundHandler<Object>() {
-            @Override
-            protected void channelRead0(ChannelHandlerContext ctx,
-                                        Object msg)
-                                                        throws Exception {
-                logger.warn("Not processed message {}", msg);
-            }
-            @Override
-            public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause)
-                            throws Exception {
-                if (cause.getCause() instanceof NotSslRecordException) {
-                    logger.warn("Not a SSL connexion from {} on SSL listen", ctx.channel().remoteAddress());
-                } else if (cause.getCause() instanceof SSLHandshakeException) {
-                    logger.warn("Failed SSL handshake from {}: {}", ctx.channel().remoteAddress(), cause.getCause().getMessage());
-                    logger.catching(Level.DEBUG, cause);
-                } else {
-                    logger.warn("Not handled exception {} from {}", Helpers.resolveThrowableException(cause), ctx.channel().remoteAddress());
-                    logger.catching(Level.WARN, cause);
-                }
-            }
-        };
-        p.addLast("errorhandler", h);
+        p.addLast("errorhandler", new ErrorHandler(logger));
     }
 
 }
