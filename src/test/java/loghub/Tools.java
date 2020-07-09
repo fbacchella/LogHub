af04b0837553a8c03822fe5115a64fcdea69fef1
Fabrice Bacchella <fabrice.bacchella@orange.fr>
2020-07-09 12:30:32 +0200

    Don't override java.io.tmpdir within tests.

--- src/test/java/loghub/Tools.java
+++ src/test/java/loghub/Tools.java
@@ -26,7 +26,6 @@ public class Tools {
     static public void configure() throws IOException {
         Locale.setDefault(new Locale("POSIX"));
         System.getProperties().setProperty("java.awt.headless","true");
-        System.setProperty("java.io.tmpdir", "tmp");
         LogUtils.configure();
     }
 
