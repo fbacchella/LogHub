ecee6a886bcfdaeabfe0c531699b8512a0ebb727
Fabrice Bacchella <fabrice.bacchella@orange.fr>
2020-10-29 21:00:55 +0100

    Adding a ArcSight CEF parser.

---
+++ src/main/java/loghub/processors/ParseCef.java
@@ -0,0 +1,82 @@
+package loghub.processors;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import loghub.Event;
+import loghub.Processor;
+import loghub.ProcessorException;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Implementations of the CEF specifications, see https://kc.mcafee.com/resources/sites/MCAFEE/content/live/CORP_KNOWLEDGEBASE/78000/KB78712/en_US/CEF_White_Paper_20100722.pdf.
+ * <p>
+ * They are totally brain dead, many corner case and missing escape missing: single | or &lt;space&gt; are allowed in extensions.
+ * 
+ * @author Fabrice Bacchella
+ *
+ */
+public class ParseCef extends Processor {
+
+    private static final String[] COLUMNS = new String[]{"version", "device_vendor", "device_product", "device_version", "device_event_class_id", "name", "severity"};
+    private static final Pattern fieldPattern = Pattern.compile("(?:[^\\|]|(?<=\\\\)\\|)+");
+    private static final Pattern extensionPattern = Pattern.compile("(?<key>[a-zA-Z0-9_]+)=(?<value>(?:[^ ]| (?![a-zA-Z0-9_]+=))+)");
+
+    @Getter @Setter
+    private String field = "message";
+    private ThreadLocal<Matcher> fieldMatcherSource = ThreadLocal.withInitial(() -> fieldPattern.matcher(""));
+    private ThreadLocal<Matcher> extensionMatcherSource = ThreadLocal.withInitial(() -> extensionPattern.matcher(""));
+
+    @Override
+    public boolean process(Event event) throws ProcessorException {
+        String message = event.get(field).toString();
+        if (! message.startsWith("CEF:")) {
+            throw event.buildException("not a CEF message: \"" + message + "\"");
+        }
+        message = message.substring(4);
+        try {
+            Map<String, Object> cefContent = new HashMap<>(COLUMNS.length);
+            // The CEF specifications are totally brain-dead, so nothing is easy, needs to be parsed with care
+            Matcher m = fieldMatcherSource.get();
+            m.reset(message);
+
+            // First resolution of the CEF fields
+            int i = -1;
+            while (++i < 7 && m.find()) {
+                String column = COLUMNS[i];
+                switch (column) {
+                case "version":
+                case "severity":
+                    try {
+                        cefContent.put(column, Integer.valueOf(m.group()));
+                    } catch (Exception e) {
+                        cefContent.put(column + "_failed", m.group());
+                    }
+                    break;
+                default:
+                    cefContent.put(column, m.group().replace("\\\\", "\\").replace("\\|", "|"));
+                }
+            }
+            String extension = message.substring(m.end() + 1);
+
+            //Resolution of extensions
+            Map<String, Object> cefExtensions = new HashMap<>();
+            Matcher m2 = extensionMatcherSource.get();
+            m2.reset(extension);
+            while (m2.find()) {
+                cefExtensions.put(m2.group("key"), m2.group("value").replace("\\\\", "\\").replace("\\=", "=").replace("\\n", "\n").replace("\\r", "\r"));
+            }
+
+            event.put("cef_fields", cefContent);
+            event.put("cef_extensions", cefExtensions);
+
+            return true;
+        } catch (Exception e) {
+            throw event.buildException("failed to parse CEF: " + message, e);
+        }
+    }
+
+}
---
+++ src/test/java/loghub/processors/TestParseCef.java
@@ -0,0 +1,65 @@
+package loghub.processors;
+
+import java.beans.IntrospectionException;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Map;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import loghub.BeanChecks;
+import loghub.Event;
+import loghub.LogUtils;
+import loghub.ProcessorException;
+import loghub.Tools;
+import loghub.configuration.Properties;
+
+public class TestParseCef {
+
+
+    private static Logger logger;
+
+    @BeforeClass
+    static public void configure() throws IOException {
+        Tools.configure();
+        logger = LogManager.getLogger();
+        LogUtils.setLevel(logger, Level.TRACE, "loghub.processors");
+    }
+
+    @Test
+    public void testCef() throws ProcessorException {
+        ParseCef parse = new ParseCef();
+        parse.setField("content");
+        Assert.assertTrue(parse.configure(new Properties(Collections.emptyMap())));
+        Event event = Tools.getEvent();
+        event.put("content", "CEF:0|security|threatmanager|1.0|100|detected a \\| in packet|10|src=10.0.0.1 act=blocked a \\\\ dst=1.1.1.1 comment=with | in it comment2=with \\= in it comment3=with \\r in it");
+        Assert.assertTrue(parse.process(event));
+        @SuppressWarnings("unchecked")
+        Map<String, Object> fields= (Map<String, Object>) event.get("cef_fields");
+        String[] fields_names = new String[]{"version", "device_vendor", "device_product", "device_version", "device_event_class_id", "name", "severity"};
+        Object[] fields_values = new Object[]{0, "security", "threatmanager", "1.0", "100", "detected a | in packet", 10};
+        for (int i=0; i < fields_names.length; i++) {
+            Assert.assertEquals(fields_values[i], fields.get(fields_names[i]));
+        }
+        @SuppressWarnings("unchecked")
+        Map<String, Object> extensions= (Map<String, Object>) event.get("cef_extensions");
+        String[] extensions_names = new String[]{"src", "act", "dst", "comment", "comment2", "comment3"};
+        Object[] extensions_values = new Object[]{"10.0.0.1", "blocked a \\", "1.1.1.1", "with | in it", "with = in it", "with \r in it"};
+        for (int i=0; i < extensions_names.length; i++) {
+            Assert.assertEquals(extensions_values[i], extensions.get(extensions_names[i]));
+        }
+    }
+
+    @Test
+    public void test_loghub_processors_ParseCsv() throws ClassNotFoundException, IntrospectionException {
+        BeanChecks.beansCheck(logger, "loghub.processors.ParseCef"
+                              ,BeanChecks.BeanInfo.build("field", String.class)
+                        );
+    }
+
+}
