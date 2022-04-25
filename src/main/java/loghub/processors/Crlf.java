662062ddea130d0885949ab2187e008ab54e8f23
Fabrice Bacchella <fabrice.bacchella@orange.fr>
2022-04-25 17:00:51 +0200

    Adding a processor that ensure that line separators within a field is normalized.

---
+++ src/main/java/loghub/processors/Crlf.java
@@ -0,0 +1,56 @@
+package loghub.processors;
+
+import java.util.Locale;
+import java.util.stream.Collectors;
+
+import loghub.Event;
+import loghub.ProcessorException;
+import lombok.Getter;
+import lombok.Setter;
+
+public class Crlf extends FieldsProcessor {
+
+    private enum Format {
+        CRLF("\r\n"),
+        CR("\r"),
+        LF("\n"),
+        KEEP("");
+        final String separator;
+
+        Format(String separator) {
+            this.separator = separator;
+        }
+    }
+
+    private Format format;
+    @Getter
+    @Setter
+    private boolean escape;
+
+    @Override
+    public Object fieldFunction(Event event, Object value) throws ProcessorException {
+        if (!(value instanceof String) || (format == Format.KEEP && !escape)) {
+            return value;
+        }
+        String message = (String) value;
+        if (format == Format.KEEP && escape) {
+            return message.replace("\r", "\\r").replace("\n", "\\n");
+        } else {
+            String separator = escape ? format.separator.replace("\r", "\\r").replace("\n", "\\n") :  format.separator ;
+            return message.lines().collect(Collectors.joining(separator));
+        }
+    }
+
+    public String getFormat() {
+        return format.name();
+    }
+
+    public void setFormat(String format) {
+        if (format == null || format.isBlank()) {
+            this.format = Format.KEEP;
+        } else {
+            this.format = Format.valueOf(format.toUpperCase(Locale.ENGLISH));
+        }
+    }
+
+}
---
+++ src/test/java/loghub/processors/TestCrlf.java
@@ -0,0 +1,74 @@
+package loghub.processors;
+
+import java.beans.IntrospectionException;
+import java.io.IOException;
+import java.util.Collections;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import loghub.BeanChecks;
+import loghub.Event;
+import loghub.LogUtils;
+import loghub.Processor;
+import loghub.ProcessorException;
+import loghub.Tools;
+import loghub.VariablePath;
+import loghub.configuration.Properties;
+
+public class TestCrlf {
+
+    private static Logger logger;
+
+    @BeforeClass
+    static public void configure() throws IOException {
+        Tools.configure();
+        logger = LogManager.getLogger();
+        LogUtils.setLevel(logger, Level.TRACE, "loghub.processors");
+    }
+
+    @Test
+    public void testConversion() throws ProcessorException {
+        test("CRlf", false, "a\rb\nc\r\nd", "a\r\nb\r\nc\r\nd");
+        test("CRlf", true, "a\rb\nc\r\nd", "a\\r\\nb\\r\\nc\\r\\nd");
+        test("CR", false, "a\rb\nc\r\nd", "a\rb\rc\rd");
+        test("CR", true, "a\rb\nc\r\nd", "a\\rb\\rc\\rd");
+        test("LF", false, "a\rb\nc\r\nd", "a\nb\nc\nd");
+        test("lf", true, "a\rb\nc\r\nd", "a\\nb\\nc\\nd");
+        test(null, false, "a\rb\nc\r\nd", "a\rb\nc\r\nd");
+        test("", true, "a\rb\nc\r\nd", "a\\rb\\nc\\r\\nd");
+        test("kEEp", true, "a\rb\nc\r\nd", "a\\rb\\nc\\r\\nd");
+    }
+
+    private void test(String format, boolean escape, String input, String output) throws ProcessorException {
+        Crlf parse = new Crlf();
+        parse.setEscape(escape);
+        parse.setFormat(format);
+        parse.setField(VariablePath.of("field"));
+        Assert.assertTrue(parse.configure(new Properties(Collections.emptyMap())));
+        Event event = Tools.getEvent();
+        event.put("field", input);
+        parse.process(event);
+        Assert.assertEquals(output, event.get("field"));
+    }
+
+    @Test
+    public void test_loghub_processors_NettyNameResolver() throws IntrospectionException, ReflectiveOperationException {
+        BeanChecks.beansCheck(logger, "loghub.processors.Crlf"
+                , BeanChecks.BeanInfo.build("format", String.class)
+                , BeanChecks.BeanInfo.build("escape", Boolean.TYPE)
+                , BeanChecks.BeanInfo.build("destination", String.class)
+                , BeanChecks.BeanInfo.build("field", VariablePath.class)
+                , BeanChecks.BeanInfo.build("fields", new Object[] {}.getClass())
+                , BeanChecks.BeanInfo.build("path", String.class)
+                , BeanChecks.BeanInfo.build("if", String.class)
+                , BeanChecks.BeanInfo.build("success", Processor.class)
+                , BeanChecks.BeanInfo.build("failure", Processor.class)
+                , BeanChecks.BeanInfo.build("exception", Processor.class)
+        );
+    }
+}
