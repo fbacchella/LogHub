304c433de36003b394199cf2b75e648b3cd609d7
Fabrice Bacchella <fabrice.bacchella@orange.fr>
2022-04-28 14:15:59 +0200

    Adding a syslog encoder, can only generate RFC5424 formatted logs for now.

---
+++ src/main/java/loghub/encoders/Syslog.java
@@ -0,0 +1,194 @@
+package loghub.encoders;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.time.ZoneId;
+import java.util.Locale;
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+import com.axibase.date.DatetimeProcessor;
+import com.axibase.date.PatternResolver;
+
+import loghub.BuilderClass;
+import loghub.Event;
+import loghub.Expression;
+import loghub.Helpers;
+import loghub.ProcessorException;
+import lombok.Setter;
+
+@BuilderClass(Syslog.Builder.class)
+public class Syslog extends Encoder {
+
+    private static final byte[] BOM = new byte[]{(byte) 0xEF, (byte) 0xBB, (byte) 0xBF};
+    private enum Format {
+        RFC5424,
+        RFC3164
+    }
+
+    public static class Builder extends Encoder.Builder<Syslog> {
+        @Setter
+        private String format = Format.RFC5424.name();
+        @Setter
+        private Expression severity = new Expression("-");
+        @Setter
+        private Expression facility = new Expression("-");;
+        @Setter
+        private int version = 1;
+        @Setter
+        private Expression hostname = new Expression("-");;
+        @Setter
+        private Expression appname = new Expression("-");;
+        @Setter
+        private Expression procid = new Expression("-");;
+        @Setter
+        private Expression msgid = new Expression("-");;
+        @Setter
+        private Expression timestamp = new Expression("-");;
+        @Setter
+        private Expression message = new Expression("-");;
+        @Setter
+        private boolean withbom = false;
+        @Setter
+        private String charset = StandardCharsets.US_ASCII.name();
+        @Setter
+        private int secFrac = 3;
+
+        @Override
+        public Syslog build() {
+            return new Syslog(this);
+        }
+    }
+
+    public static Syslog.Builder getBuilder() {
+        return new Syslog.Builder();
+    }
+
+    private final DatetimeProcessor dateFormatter;
+
+    @Setter
+    private final Format format;
+    @Setter
+    private final Charset charset;
+    @Setter
+    private final Expression severity;
+    @Setter
+    private final Expression facility;
+    @Setter
+    private final int version;
+    @Setter
+    private final Expression hostname;
+    @Setter
+    private final Expression appname;
+    @Setter
+    private final Expression procid;
+    @Setter
+    private final Expression msgid;
+    @Setter
+    private final Expression timestamp;
+    @Setter
+    private final Expression message;
+    @Setter
+    private final boolean withbom;
+
+    private Syslog(Syslog.Builder builder) {
+        super(builder);
+        this.format = Format.valueOf(builder.format.toUpperCase(Locale.ENGLISH));
+        this.charset = Charset.forName(builder.charset);
+        this.severity = builder.severity;
+        this.facility = builder.facility;
+        this.version = builder.version;
+        this.hostname = builder.hostname;
+        this.appname = builder.appname;
+        this.procid = builder.procid;
+        this.msgid = builder.msgid;
+        this.timestamp = builder.timestamp;
+        this.message = builder.message;
+        this.withbom = builder.withbom;
+        if (format == Format.RFC5424) {
+            StringBuilder timestampformat = new StringBuilder("yyyy-MM-dd'T'HH:mm:ss.");
+            IntStream.range(0, builder.secFrac).forEach(i -> timestampformat.append("S"));
+            timestampformat.append("Z");
+            dateFormatter = PatternResolver.createNewFormatter(timestampformat.toString()).withDefaultZone(ZoneId.of("UTC"));
+        } else {
+            dateFormatter = null;
+        }
+    }
+
+    @Override
+    public byte[] encode(Event event) throws EncodeException {
+        switch (format) {
+        case RFC5424:
+            return formatRfc5424(event);
+        default:
+            throw new UnsupportedOperationException("Not support syslog format: " + format.name());
+        }
+    }
+
+    private byte[] formatRfc5424(Event event) throws EncodeException {
+        try {
+            StringBuilder syslogline = new StringBuilder();
+            syslogline.append("<");
+
+            //Resoving facility
+            int realfacility;
+            Object tryfacility = facility.eval(event);
+            if (tryfacility instanceof Number) {
+                realfacility = ((Number) tryfacility).intValue();
+            } else {
+                try {
+                    realfacility = Integer.parseInt(tryfacility.toString());
+                } catch (NumberFormatException e) {
+                    throw new EncodeException("Invalid facility: " + tryfacility);
+                }
+            }
+            if (realfacility < 0 || realfacility > 23) {
+                throw new EncodeException("Invalid facility: " + tryfacility);
+            }
+
+            //Resoving severity
+            int realseverity;
+            Object tryseverity = severity.eval(event);
+            if (tryseverity instanceof Number) {
+                realseverity = ((Number) tryseverity).intValue();
+            } else {
+                try {
+                realseverity = Integer.parseInt(tryseverity.toString());
+                } catch (NumberFormatException e) {
+                    throw  new EncodeException("Invalid severity: " + tryseverity);
+                }
+            }
+            if (realseverity < 0 || realseverity > 7) {
+                throw new EncodeException("Invalid severity: " + tryseverity);
+            }
+
+            syslogline.append(realfacility * 8 + realseverity);
+            syslogline.append(">");
+            syslogline.append(version).append(" ");
+            syslogline.append(dateFormatter.print(event.getTimestamp().getTime())).append(" ");
+            syslogline.append(hostname.eval(event)).append(" ");
+            syslogline.append(appname.eval(event)).append(" ");
+            syslogline.append(procid.eval(event)).append(" ");
+            syslogline.append(msgid.eval(event)).append(" ");
+            syslogline.append("[loghub]");
+            String msg = Optional.ofNullable(message.eval(event)).orElse("").toString().trim();
+            if (!msg.isEmpty()) {
+                syslogline.append(" ");
+            }
+            if (!withbom) {
+                syslogline.append(msg);
+                return syslogline.toString().getBytes(charset);
+            } else {
+                byte[] prefix = syslogline.toString().getBytes(StandardCharsets.US_ASCII);
+                byte[] msgbytes = msg.getBytes(StandardCharsets.UTF_8);
+                ByteBuffer buffer = ByteBuffer.allocate(prefix.length + BOM.length + msgbytes.length);
+                buffer.put(prefix).put(BOM).put(msgbytes);
+                return buffer.array();
+            }
+        } catch (ProcessorException e) {
+            throw new EncodeException("Can't encode syslog message: " + Helpers.resolveThrowableException(e), e);
+        }
+    }
+
+}
---
+++ src/test/java/loghub/encoders/TestSyslog.java
@@ -0,0 +1,134 @@
+package loghub.encoders;
+
+import java.beans.IntrospectionException;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import loghub.BeanChecks;
+import loghub.Event;
+import loghub.Expression;
+import loghub.LogUtils;
+import loghub.RouteParser;
+import loghub.Tools;
+import loghub.configuration.ConfigurationTools;
+import loghub.configuration.Properties;
+import loghub.senders.InMemorySender;
+
+public class TestSyslog {
+
+    private static Logger logger;
+
+    @BeforeClass
+    static public void configure() throws IOException {
+        Tools.configure();
+        logger = LogManager.getLogger();
+        LogUtils.setLevel(logger, Level.TRACE);
+    }
+
+    @Test
+    public void testParsing() {
+        ConfigurationTools.parseFragment("output { loghub.senders.Stdout { encoder: loghub.encoders.Syslog { }}}", i -> i.output());
+        ConfigurationTools.parseFragment("output { loghub.senders.Stdout { encoder: loghub.encoders.Syslog {format: \"rfc5424\" }}}", i -> i.output());
+    }
+
+    private Syslog.Builder getSampleBuilder() {
+        Syslog.Builder builder = Syslog.getBuilder();
+        builder.setCharset("UTF-8");
+        builder.setFormat("rFc5424");
+        builder.setVersion(2);
+        // 5 and 20 are taken from the RFC and are said generate a priority of 165
+        builder.setSeverity(new Expression(5));
+        builder.setFacility(new Expression("20"));
+        builder.setHostname(new Expression("HOSTNAME"));
+        builder.setAppname(new Expression("APP-NAME"));
+        builder.setProcid(new Expression("PROCID"));
+        builder.setMsgid(new Expression("MSGID"));
+        builder.setSecFrac(4);
+        builder.setMessage(ConfigurationTools.unWrap("[message]", RouteParser::expression));
+        return builder;
+    }
+
+    @Test
+    public void testfull() throws EncodeException, Expression.ExpressionException {
+        Syslog encoder = getSampleBuilder().build();
+        Assert.assertTrue(encoder.configure(new Properties(Collections.emptyMap()), InMemorySender.getBuilder().build()));
+        Event e = Tools.getEvent();
+        e.setTimestamp(new Date(0));
+        e.put("message", "unit test");
+
+        byte[] resultbytes = encoder.encode(e);
+        String result = new String(resultbytes, StandardCharsets.US_ASCII);
+        Assert.assertEquals("<165>2 1970-01-01T00:00:00.0000Z HOSTNAME APP-NAME PROCID MSGID [loghub] unit test", result);
+    }
+
+    @Test
+    public void testbom() throws EncodeException, Expression.ExpressionException {
+        Syslog.Builder builder = getSampleBuilder();
+        builder.setWithbom(true);
+        Syslog encoder = builder.build();
+        Assert.assertTrue(encoder.configure(new Properties(Collections.emptyMap()), InMemorySender.getBuilder().build()));
+        Event e = Tools.getEvent();
+        e.setTimestamp(new Date(0));
+        e.put("message", "éèœ");
+
+        byte[] resultbytes = encoder.encode(e);
+        String prefix = new String(Arrays.copyOf(resultbytes, 73), StandardCharsets.US_ASCII);
+        Assert.assertEquals("<165>2 1970-01-01T00:00:00.0000Z HOSTNAME APP-NAME PROCID MSGID [loghub] ", prefix);
+        byte[] bom = Arrays.copyOfRange(resultbytes, 73, 76);
+        Assert.assertArrayEquals(new byte[]{(byte) 0xEF, (byte) 0xBB, (byte) 0xBF}, bom);
+        String message = new String(Arrays.copyOfRange(resultbytes, 76, resultbytes.length), StandardCharsets.UTF_8);
+        Assert.assertEquals("éèœ", message);
+    }
+
+    @Test
+    public void testbadseverity() throws EncodeException, Expression.ExpressionException {
+        Syslog.Builder builder = getSampleBuilder();
+        builder.setSeverity(new Expression(8));
+        Syslog encoder = builder.build();
+        Assert.assertTrue(encoder.configure(new Properties(Collections.emptyMap()), InMemorySender.getBuilder().build()));
+        Event e = Tools.getEvent();
+        EncodeException ex = Assert.assertThrows(EncodeException.class, () -> encoder.encode(e));
+        Assert.assertEquals("Invalid severity: 8", ex.getMessage());
+    }
+
+    @Test
+    public void testbadfacility() throws EncodeException, Expression.ExpressionException {
+        Syslog.Builder builder = getSampleBuilder();
+        builder.setFacility(new Expression(24));
+        Syslog encoder = builder.build();
+        Assert.assertTrue(encoder.configure(new Properties(Collections.emptyMap()), InMemorySender.getBuilder().build()));
+        Event e = Tools.getEvent();
+        EncodeException ex = Assert.assertThrows(EncodeException.class, () -> encoder.encode(e));
+        Assert.assertEquals("Invalid facility: 24", ex.getMessage());
+    }
+
+    @Test
+    public void test_loghub_processors_Syslog() throws IntrospectionException, ReflectiveOperationException {
+        BeanChecks.beansCheck(logger, "loghub.encoders.Syslog"
+                , BeanChecks.BeanInfo.build("format", String.class)
+                , BeanChecks.BeanInfo.build("severity", Expression.class)
+                , BeanChecks.BeanInfo.build("facility", Expression.class)
+                , BeanChecks.BeanInfo.build("version", Integer.TYPE)
+                , BeanChecks.BeanInfo.build("hostname", Expression.class)
+                , BeanChecks.BeanInfo.build("appname", Expression.class)
+                , BeanChecks.BeanInfo.build("procid", Expression.class)
+                , BeanChecks.BeanInfo.build("msgid", Expression.class)
+                , BeanChecks.BeanInfo.build("timestamp", Expression.class)
+                , BeanChecks.BeanInfo.build("secFrac", Integer.TYPE)
+                , BeanChecks.BeanInfo.build("message", Expression.class)
+                , BeanChecks.BeanInfo.build("withbom", Boolean.TYPE)
+                , BeanChecks.BeanInfo.build("charset", String.class)
+        );
+    }
+
+}
