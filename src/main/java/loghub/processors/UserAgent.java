ab35007be3ad8720134346e3a27e2f4b649d129f
Fabrice Bacchella <fabrice.bacchella@orange.fr>
2022-07-04 15:22:26 +0200

    Updated UserAgent processor.

--- src/main/java/loghub/processors/UserAgent.java
+++ src/main/java/loghub/processors/UserAgent.java
@@ -1,52 +1,109 @@
 package loghub.processors;
 
 import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.io.UncheckedIOException;
+import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
+import org.apache.logging.log4j.Level;
+
+import loghub.BuilderClass;
 import loghub.CachingParser;
 import loghub.Event;
 import loghub.Helpers;
 import loghub.configuration.Properties;
+import lombok.Getter;
+import lombok.Setter;
 import ua_parser.Client;
 import ua_parser.Parser;
 
+@BuilderClass(UserAgent.Builder.class)
 public class UserAgent extends FieldsProcessor {
 
+    public static class Builder extends FieldsProcessor.Builder<UserAgent> {
+        @Setter
+        private int cacheSize = 1000;
+        @Setter
+        private String agentsFile = null;
+        @Setter
+        private String agentsUrl = null;
+        public UserAgent build() {
+            return new UserAgent(this);
+        }
+    }
+    public static UserAgent.Builder getBuilder() {
+        return new UserAgent.Builder();
+    }
+
     private Parser uaParser;
-    private int cacheSize = 1000;
-    private String agentsFile = null;
-    private URL agentsUrl = null;
+    @Getter
+    private final int cacheSize;
+    @Getter
+    private final URI agentsUrl;
+
+    private UserAgent(Builder builder) {
+        super(builder);
+        agentsUrl = Optional.ofNullable(builder.agentsFile)
+                            .or(() -> Optional.ofNullable(builder.agentsUrl))
+                            .map(Helpers::FileUri)
+                            .orElse(null);
+        cacheSize = builder.cacheSize;
+    }
+
+    @Override
+    public boolean configure(Properties properties) {
+        try (InputStream is = new BufferedInputStream(resolveRegexesLocation(properties))) {
+            uaParser = new CachingParser(cacheSize, properties, is);
+            return super.configure(properties);
+        } catch (IOException | UncheckedIOException | IllegalArgumentException ex) {
+            logger.error("Failed to read user agents regexes: {}", () -> Helpers.resolveThrowableException(ex));
+            logger.catching(Level.DEBUG, ex);
+            return false;
+        }
+    }
+
+    private InputStream resolveRegexesLocation(Properties properties) {
+        InputStream is = Optional.ofNullable(agentsUrl).map(u -> {
+            try {
+                return u.toURL().openStream();
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+        }).orElseGet(() -> properties.classloader.getResourceAsStream("ua_parser/regexes.yaml"));
+        if (is == null) {
+            throw new IllegalArgumentException("User agents regexes not found");
+        } else {
+            return is;
+        }
+    }
 
     @Override
     public Object fieldFunction(Event event, Object value) {
         Client c = uaParser.parse(value.toString());
 
         Map<String, Object> ua = new HashMap<>(3);
-        Helpers.putNotEmpty(ua, "device", c.device.family);
+        Optional.of(c.device.family).ifPresent(v -> ua.put("device", v));
 
         Map<String, Object> os =  new HashMap<>(5);
-        Helpers.putNotEmpty(os, "family", c.os.family);
-        Helpers.putNotEmpty(os, "major", c.os.major);
-        Helpers.putNotEmpty(os, "minor", c.os.minor);
-        Helpers.putNotEmpty(os, "patch", c.os.patch);
-        Helpers.putNotEmpty(os, "patchMinor", c.os.patchMinor);
-        if(os.size() > 0) {
+        Optional.ofNullable(c.os.family).ifPresent(v -> os.put("family", v));
+        Optional.ofNullable(c.os.major).ifPresent(v -> os.put("major", v));
+        Optional.ofNullable(c.os.minor).ifPresent(v -> os.put("minor", v));
+        Optional.ofNullable(c.os.patch).ifPresent(v -> os.put("patch", v));
+        Optional.ofNullable(c.os.patchMinor).ifPresent(v -> os.put("patchMinor", v));
+        if (! os.isEmpty()) {
             ua.put("os", os);
         }
 
         Map<String, Object> agent =  new HashMap<>(4);
-        Helpers.putNotEmpty(agent, "family", c.userAgent.family);
-        Helpers.putNotEmpty(agent, "major", c.userAgent.major);
-        Helpers.putNotEmpty(agent, "minor", c.userAgent.minor);
-        Helpers.putNotEmpty(agent, "patch", c.userAgent.patch);
-        if(agent.size() > 0) {
+        Optional.ofNullable(c.userAgent.family).ifPresent(v -> agent.put("family", v));
+        Optional.ofNullable(c.userAgent.major).ifPresent(v -> agent.put("major", v));
+        Optional.ofNullable(c.userAgent.minor).ifPresent(v -> agent.put("minor", v));
+        Optional.ofNullable(c.userAgent.patch).ifPresent(v -> agent.put("patch", v));
+        if (! agent.isEmpty()) {
             ua.put("userAgent", agent);
         }
 
@@ -56,87 +113,6 @@ public class UserAgent extends FieldsProcessor {
     @Override
     public String getName() {
         return null;
-    }
-
-    @Override
-    public boolean configure(Properties properties) {
-        InputStream is;
-        if (agentsUrl != null) {
-            try {
-                is = agentsUrl.openStream();
-            } catch (IOException e) {
-                logger.error("URL {} can't be used as user-agent regexes source: {}", agentsUrl, e.getMessage());
-                return false;
-            }
-        } else if (agentsFile != null && ! agentsFile.isEmpty()) {
-            try {
-                is = new FileInputStream(agentsFile);
-            } catch (FileNotFoundException e) {
-                logger.error("File {} can't be used as user-agent regexes source: {}", agentsFile, e.getMessage());
-                return false;
-            }
-        } else {
-            is = properties.classloader.getResourceAsStream("ua_parser/regexes.yaml");
-            if (is == null) {
-                logger.error("ua_parser/regexes.yaml is not in the classpath or can't be loaded");
-                return false;
-            }
-        }
-        is = new BufferedInputStream(is);
-        uaParser = new CachingParser(cacheSize, properties, is);
-        try {
-            is.close();
-        } catch (IOException e) {
-            logger.error("close failure");
-            return false;
-        }
-        return super.configure(properties);
-    }
-
-    /**
-     * @return the cacheSize
-     */
-    public int getCacheSize() {
-        return cacheSize;
-    }
-
-    /**
-     * @param cacheSize the cacheSize to set
-     */
-    public void setCacheSize(int cacheSize) {
-        this.cacheSize = cacheSize;
-    }
-
-    /**
-     * @return the agentsFile
-     */
-    public String getAgentsFile() {
-        return agentsFile == null ? "" : agentsFile;
-    }
-
-    /**
-     * @param agentsFile the agentsFile to set
-     */
-    public void setAgentsFile(String agentsFile) {
-        this.agentsFile = agentsFile;
-    }
-
-    /**
-     * @return the agentsUrl
-     */
-    public String getAgentsUrl() {
-        return agentsUrl == null ? "" : agentsUrl.toString();
-    }
-
-    /**
-     * @param agentsUrl An URL where the regexes file can be found (like the master file https://raw.githubusercontent.com/ua-parser/uap-core/master/regexes.yaml)
-     */
-    public void setAgentsUrl(String agentsUrl) {
-        try {
-            this.agentsUrl = new URL(agentsUrl);
-        } catch (MalformedURLException e) {
-            throw new RuntimeException("can't parse URL " + agentsUrl, e);
-        }
     }
 
 }
--- src/test/java/loghub/processors/TestUserAgent.java
+++ src/test/java/loghub/processors/TestUserAgent.java
@@ -1,5 +1,6 @@
 package loghub.processors;
 
+import java.beans.IntrospectionException;
 import java.io.IOException;
 import java.util.Collections;
 
@@ -10,16 +11,20 @@ import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import loghub.BeanChecks;
 import loghub.Event;
 import loghub.Event.Action;
+import loghub.Expression;
 import loghub.LogUtils;
+import loghub.Processor;
 import loghub.ProcessorException;
 import loghub.Tools;
+import loghub.VarFormatter;
 import loghub.VariablePath;
 import loghub.configuration.Properties;
 
 public class TestUserAgent {
-    
+
     private static Logger logger;
 
     @BeforeClass
@@ -31,10 +36,12 @@ public class TestUserAgent {
 
     @Test
     public void test1() throws ProcessorException {
-        UserAgent ua = new UserAgent();
-        ua.setField(VariablePath.of(new String[] {"User-Agent"}));
-        ua.setCacheSize(10);
-        ua.setDestination(VariablePath.of("agent"));
+        UserAgent.Builder builder = UserAgent.getBuilder();
+        builder.setField(VariablePath.of(new String[] {"User-Agent"}));
+        builder.setCacheSize(10);
+        builder.setDestination(VariablePath.of("agent"));
+
+        UserAgent ua = builder.build();
         Assert.assertTrue("configuration failed", ua.configure(new Properties(Collections.emptyMap())));
 
         String uaString = "Mozilla/5.0 (iPhone; CPU iPhone OS 5_1_1 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9B206 Safari/7534.48.3";
@@ -42,18 +49,40 @@ public class TestUserAgent {
         Event event = Tools.getEvent();
         event.put("User-Agent", uaString);
         Assert.assertTrue(ua.process(event));
+        System.out.println(event);
         Object family = event.applyAtPath(Action.GET, VariablePath.of(new String[] {"agent", "userAgent", "family"}), null, false);
         Assert.assertEquals("can't find user agent parsing", "Mobile Safari", family);
+        Object osfamilly = event.applyAtPath(Action.GET, VariablePath.of(new String[] {"agent", "os", "family"}), null, false);
+        Assert.assertEquals("can't find user agent parsing", "iOS", osfamilly);
     }
 
     @Test
-    public void testDownload() throws ProcessorException {
-        UserAgent ua = new UserAgent();
-        ua.setField(VariablePath.of(new String[] {"User-Agent"}));
-        ua.setCacheSize(10);
-        ua.setDestination(VariablePath.of("agent"));
-        ua.setAgentsUrl(TestUserAgent.class.getClassLoader().getResource("regexes.yaml").toString());
+    public void testDownload() {
+        UserAgent.Builder builder = UserAgent.getBuilder();
+        builder.setField(VariablePath.of(new String[] {"User-Agent"}));
+        builder.setCacheSize(10);
+        builder.setDestination(VariablePath.of("agent"));
+        builder.setAgentsUrl(TestUserAgent.class.getClassLoader().getResource("regexes.yaml").toString());
+        UserAgent ua = builder.build();
         Assert.assertTrue("configuration failed", ua.configure(new Properties(Collections.emptyMap())));
+    }
+
+    @Test
+    public void test_loghub_processors_UserAgent() throws IntrospectionException, ReflectiveOperationException {
+        BeanChecks.beansCheck(logger, "loghub.processors.UserAgent"
+                , BeanChecks.BeanInfo.build("cacheSize", Integer.TYPE)
+                , BeanChecks.BeanInfo.build("agentsFile", String.class)
+                , BeanChecks.BeanInfo.build("agentsUrl", String.class)
+                , BeanChecks.BeanInfo.build("destination", VariablePath.class)
+                , BeanChecks.BeanInfo.build("destinationTemplate", VarFormatter.class)
+                , BeanChecks.BeanInfo.build("field", VariablePath.class)
+                , BeanChecks.BeanInfo.build("fields", Object[].class)
+                , BeanChecks.BeanInfo.build("path", VariablePath.class)
+                , BeanChecks.BeanInfo.build("if", Expression.class)
+                , BeanChecks.BeanInfo.build("success", Processor.class)
+                , BeanChecks.BeanInfo.build("failure", Processor.class)
+                , BeanChecks.BeanInfo.build("exception", Processor.class)
+        );
     }
 
 }
