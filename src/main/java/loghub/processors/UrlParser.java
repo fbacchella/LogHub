e6331d3b1a34ff8d2d80696d5fec3f8fef32dc64
Fabrice Bacchella <fabrice.bacchella@orange.fr>
2023-04-19 14:08:10 +0200

    Useless UriParts processor, duplicate UrlParser. Improved UrlParser.

--- src/main/java/loghub/processors/UrlParser.java
+++ src/main/java/loghub/processors/UrlParser.java
@@ -1,11 +1,14 @@
 package loghub.processors;
 
 import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
 import loghub.BuilderClass;
+import loghub.Helpers;
 import loghub.ProcessorException;
 import loghub.events.Event;
 import lombok.Setter;
@@ -34,31 +37,44 @@ public class UrlParser extends FieldsProcessor {
 
     @Override
     public Object fieldFunction(Event event, Object value) throws ProcessorException {
+        URI valueUri;
         try {
-            Map<String, Object> urlInformations = new HashMap<>(9);
-            String valueStr = value.toString();
-            URI uri = refUrl.map(u -> u.resolve(valueStr)).orElseGet(() -> URI.create(valueStr));
-            Optional.ofNullable(uri.getScheme()).ifPresent(s -> urlInformations.put("scheme", s));
-            Optional.ofNullable(uri.getUserInfo()).ifPresent(ui -> {
-                int pos = ui.indexOf(':');
-                if (pos < 0) {
-                    urlInformations.put("user", ui);
-                } else {
-                    String user = ui.substring(0, pos);
-                    String password = ui.substring(pos + 1);
-                    urlInformations.put("user", user);
-                    urlInformations.put("password", password);
-                }
-            });
-            Optional.ofNullable(uri.getHost()).ifPresent(s -> urlInformations.put("domain", s));
-            Optional.of(uri.getPort()).filter(l -> l > 0).ifPresent(s -> urlInformations.put("port", s));
-            Optional.ofNullable(uri.getPath()).ifPresent(s -> urlInformations.put("path", s));
-            Optional.ofNullable(uri.getQuery()).ifPresent(s -> urlInformations.put("query", s));
-            Optional.ofNullable(uri.getFragment()).ifPresent(s -> urlInformations.put("fragment", s));
-            urlInformations.put("path", uri.getPath());
-            return urlInformations;
-        } catch (IllegalArgumentException e) {
-            throw event.buildException("failed to parse URL " + value, e);
+            if (value instanceof URI) {
+                valueUri = (URI) value;
+            } else if (value instanceof URL) {
+                valueUri = ((URL) value).toURI();
+            } else {
+                valueUri = new URI(value.toString());
+            }
+        } catch (URISyntaxException ex) {
+            throw new ProcessorException(event, value + "Not a valid URL:" + Helpers.resolveThrowableException(ex), ex);
         }
+        Map<String, Object> urlInformations = new HashMap<>(9);
+        URI uri = refUrl.map(u -> u.resolve(valueUri)).orElse(valueUri);
+        Optional.ofNullable(uri.getScheme()).ifPresent(s -> urlInformations.put("scheme", s));
+        Optional.ofNullable(uri.getUserInfo()).ifPresent(ui -> {
+            urlInformations.put("user_info", ui);
+            int colonIndex = ui.indexOf(":");
+            if (colonIndex >= 0) {
+                urlInformations.put("username", ui.substring(0, colonIndex));
+                urlInformations.put("password", ui.substring(colonIndex + 1));
+            }
+        });
+        Optional.ofNullable(uri.getHost()).ifPresent(s -> urlInformations.put("domain", s));
+        Optional.of(uri.getPort()).filter(p -> p > 0).ifPresent(s -> urlInformations.put("port", s));
+        Optional.ofNullable(uri.getPath()).ifPresent(s -> urlInformations.put("path", s));
+        Optional.ofNullable(uri.getQuery()).ifPresent(s -> urlInformations.put("query", s));
+        Optional.ofNullable(uri.getFragment()).ifPresent(s -> urlInformations.put("fragment", s));
+        Optional.ofNullable(uri.getPath()).ifPresent(path -> {
+            urlInformations.put("path", path);
+            if (path.contains(".")) {
+                int periodIndex = path.lastIndexOf('.');
+                Optional.of(path).filter(p -> periodIndex < (p.length() - 1)).ifPresent(p -> urlInformations.put("extension", p.substring(periodIndex + 1)));
+            }
+        });
+        if (uri.isOpaque()) {
+            Optional.ofNullable(uri.getSchemeSpecificPart()).ifPresent(s -> urlInformations.put("specific", s));
+        }
+        return urlInformations;
     }
 }
--- src/test/java/loghub/processors/TestUrlParser.java
+++ src/test/java/loghub/processors/TestUrlParser.java
@@ -16,9 +16,12 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import loghub.BeanChecks;
+import loghub.Expression;
 import loghub.LogUtils;
+import loghub.Processor;
 import loghub.ProcessorException;
 import loghub.Tools;
+import loghub.VarFormatter;
 import loghub.VariablePath;
 import loghub.configuration.Properties;
 import loghub.events.Event;
@@ -82,7 +85,7 @@ public class TestUrlParser {
         Assert.assertEquals("ref", values.get("fragment"));
         Assert.assertEquals(80, values.get("port"));
         Assert.assertEquals("arg", values.get("query"));
-        Assert.assertEquals("login", values.get("user"));
+        Assert.assertEquals("login", values.get("username"));
         Assert.assertEquals("pass", values.get("password"));
     }
 
@@ -97,11 +100,221 @@ public class TestUrlParser {
         Assert.assertThrows(ProcessorException.class, () -> parser.process(event));
     }
 
+    private void testContent(Map<String, Object> uriParts) {
+        Assert.assertEquals("/foo.gif", uriParts.get("path"));
+        Assert.assertEquals("frag", uriParts.get("fragment"));
+        Assert.assertEquals("gif", uriParts.get("extension"));
+        Assert.assertEquals("http", uriParts.get("scheme"));
+        Assert.assertEquals(80, uriParts.get("port"));
+        Assert.assertEquals("myusername:mypassword", uriParts.get("user_info"));
+        Assert.assertEquals("www.example.com", uriParts.get("domain"));
+        Assert.assertEquals("key1=val1&key2=val2", uriParts.get("query"));
+        Assert.assertEquals("myusername", uriParts.get("username"));
+        Assert.assertEquals("mypassword", uriParts.get("password"));
+    }
+
+    private Event runTest(Consumer<UrlParser.Builder> configure, String field, String uriString) throws ProcessorException {
+        UrlParser.Builder builder = UrlParser.getBuilder();
+        configure.accept(builder);
+        UrlParser parts = builder.build();
+        Assert.assertTrue(parts.configure(new Properties(Collections.emptyMap())));
+
+        Event event = factory.newEvent();
+        event.put(field, uriString);
+        Assert.assertTrue(parts.process(event));
+        return event;
+    }
+
+    @Test
+    public void test1() throws ProcessorException {
+        Event event = runTest(b -> {
+                    b.setField(VariablePath.of(new String[] {"input_field"}));
+                    b.setDestination(VariablePath.of("url"));
+                }, "input_field",
+                "http://myusername:mypassword@www.example.com:80/foo.gif?key1=val1&key2=val2#frag");
+        @SuppressWarnings("unchecked")
+        Map<String, Object> uriParts = (Map<String, Object>) event.get("url");
+        testContent(uriParts);
+    }
+
+    @Test
+    public void test2() throws ProcessorException {
+        Event event = runTest(b -> {
+                    b.setField(VariablePath.of(new String[] {"original"}));
+                    b.setInPlace(true);
+                }, "original",
+                "http://myusername:mypassword@www.example.com:80/foo.gif?key1=val1&key2=val2#frag");
+        testContent(event);
+    }
+
+    @Test
+    public void testExtention1() throws ProcessorException {
+        Event event = runTest(b -> {
+                    b.setField(VariablePath.of(new String[] {"original"}));
+                    b.setInPlace(true);
+                }, "original",
+                "http://myusername:mypassword@www.example.com:80/foo?key1=val1&key2=val2#frag");
+        Assert.assertEquals("/foo", event.get("path"));
+        Assert.assertFalse(event.containsKey("extension"));
+    }
+
+    @Test
+    public void testExtention2() throws ProcessorException {
+        Event event = runTest(b -> {
+                    b.setField(VariablePath.of(new String[] {"original"}));
+                    b.setInPlace(true);
+                }, "original",
+                "http://myusername:mypassword@www.example.com:80/foo.?key1=val1&key2=val2#frag");
+        Assert.assertEquals("/foo.", event.get("path"));
+        Assert.assertFalse(event.containsKey("extension"));
+    }
+
+    @Test
+    public void testExtention3() throws ProcessorException {
+        Event event = runTest(b -> {
+                    b.setField(VariablePath.of(new String[] {"original"}));
+                    b.setInPlace(true);
+                }, "original",
+                "http://myusername:mypassword@www.example.com:80/.gif?key1=val1&key2=val2#frag");
+        Assert.assertEquals("/.gif", event.get("path"));
+        Assert.assertEquals("gif", event.get("extension"));
+    }
+
+    @Test
+    public void testUserInfo1() throws ProcessorException {
+        Event event = runTest(b -> {
+                    b.setField(VariablePath.of(new String[] {"original"}));
+                    b.setInPlace(true);
+                }, "original",
+                "http://myusername@www.example.com:80/foo.gif?key1=val1&key2=val2#frag");
+        Assert.assertEquals("myusername", event.get("user_info"));
+        Assert.assertFalse(event.containsKey("password"));
+        Assert.assertFalse(event.containsKey("username"));
+    }
+
+    @Test
+    public void testUserInfo2() throws ProcessorException {
+        Event event = runTest(b -> {
+                    b.setField(VariablePath.of(new String[] {"original"}));
+                    b.setInPlace(true);
+                }, "original",
+                "http://myusername:@www.example.com:80/foo.gif?key1=val1&key2=val2#frag");
+        Assert.assertEquals("myusername", event.get("username"));
+        Assert.assertEquals("", event.get("password"));
+    }
+
+    @Test
+    public void testUserInfo3() throws ProcessorException {
+        Event event = runTest(b -> {
+                    b.setField(VariablePath.of(new String[] {"original"}));
+                    b.setInPlace(true);
+                }, "original",
+                "http://:password@www.example.com:80/foo.gif?key1=val1&key2=val2#frag");
+        Assert.assertEquals("", event.get("username"));
+        Assert.assertEquals("password", event.get("password"));
+    }
+
+    @Test
+    public void testRfcExample1() throws ProcessorException {
+        Event event = runTest(b -> {
+                    b.setField(VariablePath.of(new String[] {"original"}));
+                    b.setInPlace(true);
+                }, "original",
+                "ftp://ftp.is.co.za/rfc/rfc1808.txt");
+        System.err.println(event);
+        Assert.assertEquals("ftp", event.get("scheme"));
+    }
+
+    @Test
+    public void testRfcExample3() throws ProcessorException {
+        Event event = runTest(b -> {
+                    b.setField(VariablePath.of(new String[] {"original"}));
+                    b.setInPlace(true);
+                }, "original",
+                "ldap://[2001:db8::7]/c=GB?objectClass?one");
+        System.err.println(event);
+        Assert.assertEquals("ldap", event.get("scheme"));
+        Assert.assertEquals("[2001:db8::7]", event.get("domain"));
+        Assert.assertEquals("objectClass?one", event.get("query"));
+        Assert.assertEquals("/c=GB", event.get("path"));
+    }
+
+    @Test
+    public void testRfcExample4() throws ProcessorException {
+        Event event = runTest(b -> {
+                    b.setField(VariablePath.of(new String[] {"original"}));
+                    b.setInPlace(true);
+                }, "original",
+                "mailto:John.Doe@example.com");
+        System.err.println(event);
+        Assert.assertEquals("mailto", event.get("scheme"));
+        Assert.assertEquals("John.Doe@example.com", event.get("specific"));
+    }
+
+    @Test
+    public void testRfcExample5() throws ProcessorException {
+        Event event = runTest(b -> {
+                    b.setField(VariablePath.of(new String[] {"original"}));
+                    b.setInPlace(true);
+                }, "original",
+                "news:comp.infosystems.www.servers.unix");
+        System.err.println(event);
+        Assert.assertEquals("news", event.get("scheme"));
+        Assert.assertEquals("comp.infosystems.www.servers.unix", event.get("specific"));
+    }
+
+    @Test
+    public void testRfcExample6() throws ProcessorException {
+        Event event = runTest(b -> {
+                    b.setField(VariablePath.of(new String[] {"original"}));
+                    b.setInPlace(true);
+                }, "original",
+                "tel:+1-816-555-1212");
+        System.err.println(event);
+        Assert.assertEquals("tel", event.get("scheme"));
+        Assert.assertEquals("+1-816-555-1212", event.get("specific"));
+    }
+
+    @Test
+    public void testRfcExample7() throws ProcessorException {
+        Event event = runTest(b -> {
+                    b.setField(VariablePath.of(new String[] {"original"}));
+                    b.setInPlace(true);
+                }, "original",
+                "telnet://192.0.2.16:80/");
+        System.err.println(event);
+        Assert.assertEquals("telnet", event.get("scheme"));
+        Assert.assertEquals(80, event.get("port"));
+        Assert.assertEquals("192.0.2.16", event.get("domain"));
+        Assert.assertEquals("/", event.get("path"));
+    }
+
+    @Test
+    public void testRfcExample8() throws ProcessorException {
+        Event event = runTest(b -> {
+                    b.setField(VariablePath.of(new String[] {"original"}));
+                    b.setInPlace(true);
+                }, "original",
+                "urn:oasis:names:specification:docbook:dtd:xml:4.1.2");
+        System.err.println(event);
+        Assert.assertEquals("urn", event.get("scheme"));
+        Assert.assertEquals("oasis:names:specification:docbook:dtd:xml:4.1.2", event.get("specific"));
+    }
+
     @Test
     public void test_loghub_processors_UrlParser() throws IntrospectionException, ReflectiveOperationException {
         BeanChecks.beansCheck(logger, "loghub.processors.UrlParser"
                 , BeanChecks.BeanInfo.build("reference", String.class)
                 , BeanChecks.BeanInfo.build("inPlace", Boolean.TYPE)
+                , BeanChecks.BeanInfo.build("destination", VariablePath.class)
+                , BeanChecks.BeanInfo.build("destinationTemplate", VarFormatter.class)
+                , BeanChecks.BeanInfo.build("field", VariablePath.class)
+                , BeanChecks.BeanInfo.build("fields", Object[].class)
+                , BeanChecks.BeanInfo.build("path", VariablePath.class)
+                , BeanChecks.BeanInfo.build("if", Expression.class)
+                , BeanChecks.BeanInfo.build("success", Processor.class)
+                , BeanChecks.BeanInfo.build("failure", Processor.class)
+                , BeanChecks.BeanInfo.build("exception", Processor.class)
         );
     }
 
