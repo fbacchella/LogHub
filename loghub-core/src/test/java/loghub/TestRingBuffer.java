2d0990add716aabc1ef49ceaf9fd636677e4957d
Fabrice Bacchella <fabrice.bacchella@orange.fr>
2023-12-21 18:33:41 +0100

    RingBuffer with paddings works.

--- loghub-core/src/test/java/loghub/TestRingBuffer.java
+++ loghub-core/src/test/java/loghub/TestRingBuffer.java
@@ -13,60 +13,65 @@ import loghub.ThreadBuilder;
 
 public class TestRingBuffer {
 
-    @Test
+    @Test(timeout = 5000)
     public void testSimple() throws InterruptedException {
-        RingBuffer<AtomicInteger> rb = new RingBuffer<>(100, AtomicInteger.class, AtomicInteger::new, i -> i.set(Integer.MIN_VALUE));
+        RingBuffer<AtomicInteger> rb = new RingBuffer<>(16, AtomicInteger.class, AtomicInteger::new, i -> i.set(Integer.MIN_VALUE));
         Assert.assertTrue(rb.isEmpty());
         Assert.assertEquals(null, rb.peek(AtomicInteger::get));
-        for (AtomicInteger i = new AtomicInteger(0); i.get() < 128; i.incrementAndGet()) {
+        for (AtomicInteger i = new AtomicInteger(0); i.get() < 156; i.incrementAndGet()) {
             Assert.assertTrue(rb.put(ai -> ai.set(i.get())));
         }
-        Assert.assertFalse(rb.put( i-> i.set(Integer.MAX_VALUE), 1, TimeUnit.MILLISECONDS));
-        for (int i = 0; i < 128; i++ ) {
+        boolean putted = rb.put( i-> i.set(Integer.MAX_VALUE), 1, TimeUnit.MILLISECONDS);
+        Assert.assertFalse(putted);
+        for (int i = 0; i < 156; i++ ) {
             Integer io = rb.take(AtomicInteger::get);
             Assert.assertEquals(i, io.intValue());
         }
         Assert.assertTrue(rb.isEmpty());
     }
 
-    @Test(timeout = 5000)
+    @Test//(timeout = 5000)
     public void concurrent() throws InterruptedException {
         AtomicReference<Throwable> failed1 = new AtomicReference<>(null);
         AtomicReference<Throwable> failed2 = new AtomicReference<>(null);
         AtomicReference<Throwable> failed3 = new AtomicReference<>(null);
-        RingBuffer<AtomicInteger> rb = new RingBuffer<>(100, AtomicInteger.class, AtomicInteger::new, i -> i.set(Integer.MIN_VALUE));
-        Thread t1 = ThreadBuilder.get().setTask(() -> {
+        RingBuffer<AtomicInteger> rb = new RingBuffer<>(16, AtomicInteger.class, () -> new AtomicInteger(Integer.MIN_VALUE), i -> i.set(Integer.MIN_VALUE));
+
+        Thread t1 = ThreadBuilder.get().setName("PutterThread").setTask(() -> {
             try {
                 for (AtomicInteger i = new AtomicInteger(0); i.get() < 1000; i.incrementAndGet()) {
                     Assert.assertTrue(rb.put(ai -> ai.set(i.get())));
                 }
             } catch (Throwable e) {
+                e.printStackTrace();
                 failed2.set(e);
             }
         }).build(true);
-        Thread t2 = ThreadBuilder.get().setTask(() -> {
-            for (int i = 0; i < 1000; i++ ) {
-                try {
+        Thread t2 = ThreadBuilder.get().setName("TakerThread").setTask(() -> {
+            try {
+                for (int i = 0; i < 1000; i++ ) {
                     Integer io = rb.take(AtomicInteger::get);
                     Assert.assertEquals(i, io.intValue());
-                } catch (Throwable e) {
-                    failed2.set(e);
                 }
+            } catch (Throwable e) {
+                e.printStackTrace();
+                failed2.set(e);
             }
         }).build(true);
-        Thread t3 = ThreadBuilder.get().setTask(() -> {
-            for (int i = 0; i < 1000; i++ ) {
-                try {
+        Thread t3 = ThreadBuilder.get().setName("WatcherThread").setTask(() -> {
+            try {
+                for (int i = 0; i < 1000; i++ ) {
                     rb.peek(ai -> ai.get());
                     Thread.yield();
-                } catch (Throwable e) {
-                    failed3.set(e);
                 }
+            } catch (Throwable e) {
+                e.printStackTrace();
+                failed3.set(e);
             }
         }).build(true);
-        t1.join();
-        t2.join();
-        t3.join();
+        t1.join(1000, 0);
+        t2.join(1000, 0);
+        t3.join(1000, 0);
         for (AtomicReference<Throwable> art: List.of(failed1, failed2, failed3)) {
             Optional.ofNullable(art.get()).ifPresent(Throwable::printStackTrace);
             Assert.assertNull(art.get());
