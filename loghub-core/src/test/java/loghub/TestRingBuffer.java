939cedd59781f2276a865dd448eddb6a7e9ea3ed
Fabrice Bacchella <fabrice.bacchella@orange.fr>
2023-12-29 19:02:07 +0100

    Simplified RingBuffer API.

--- loghub-core/src/test/java/loghub/TestRingBuffer.java
+++ loghub-core/src/test/java/loghub/TestRingBuffer.java
@@ -3,7 +3,6 @@ package loghub.queue;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
 import org.junit.Assert;
@@ -15,16 +14,16 @@ public class TestRingBuffer {
 
     @Test(timeout = 5000)
     public void testSimple() throws InterruptedException {
-        RingBuffer<AtomicInteger> rb = new RingBuffer<>(16, AtomicInteger.class, AtomicInteger::new, i -> i.set(Integer.MIN_VALUE));
+        RingBuffer<Integer> rb = new RingBuffer<>(10);
         Assert.assertTrue(rb.isEmpty());
-        Assert.assertEquals(null, rb.peek(AtomicInteger::get));
-        for (AtomicInteger i = new AtomicInteger(0); i.get() < 156; i.incrementAndGet()) {
-            Assert.assertTrue(rb.put(ai -> ai.set(i.get())));
+        Assert.assertNull(rb.peek());
+        for (int i = 0; i < 16; i++) {
+            Assert.assertTrue(rb.put(i));
         }
-        boolean putted = rb.put( i-> i.set(Integer.MAX_VALUE), 1, TimeUnit.MILLISECONDS);
+        boolean putted = rb.put(Integer.MAX_VALUE, 1, TimeUnit.MILLISECONDS);
         Assert.assertFalse(putted);
-        for (int i = 0; i < 156; i++ ) {
-            Integer io = rb.take(AtomicInteger::get);
+        for (int i = 0; i < 16; i++ ) {
+            Integer io = rb.take();
             Assert.assertEquals(i, io.intValue());
         }
         Assert.assertTrue(rb.isEmpty());
@@ -35,37 +34,34 @@ public class TestRingBuffer {
         AtomicReference<Throwable> failed1 = new AtomicReference<>(null);
         AtomicReference<Throwable> failed2 = new AtomicReference<>(null);
         AtomicReference<Throwable> failed3 = new AtomicReference<>(null);
-        RingBuffer<AtomicInteger> rb = new RingBuffer<>(16, AtomicInteger.class, () -> new AtomicInteger(Integer.MIN_VALUE), i -> i.set(Integer.MIN_VALUE));
+        RingBuffer<Integer> rb = new RingBuffer<>(16);
 
         Thread t1 = ThreadBuilder.get().setName("PutterThread").setTask(() -> {
             try {
-                for (AtomicInteger i = new AtomicInteger(0); i.get() < 1000; i.incrementAndGet()) {
-                    Assert.assertTrue(rb.put(ai -> ai.set(i.get())));
+                for (int i = 0; i < 1000; i++) {
+                    Assert.assertTrue(rb.put(i));
                 }
             } catch (Throwable e) {
-                e.printStackTrace();
                 failed2.set(e);
             }
         }).build(true);
         Thread t2 = ThreadBuilder.get().setName("TakerThread").setTask(() -> {
             try {
                 for (int i = 0; i < 1000; i++ ) {
-                    Integer io = rb.take(AtomicInteger::get);
+                    Integer io = rb.take();
                     Assert.assertEquals(i, io.intValue());
                 }
             } catch (Throwable e) {
-                e.printStackTrace();
                 failed2.set(e);
             }
         }).build(true);
         Thread t3 = ThreadBuilder.get().setName("WatcherThread").setTask(() -> {
             try {
                 for (int i = 0; i < 1000; i++ ) {
-                    rb.peek(ai -> ai.get());
+                    rb.peek();
                     Thread.yield();
                 }
             } catch (Throwable e) {
-                e.printStackTrace();
                 failed3.set(e);
             }
         }).build(true);
@@ -75,18 +71,6 @@ public class TestRingBuffer {
         for (AtomicReference<Throwable> art: List.of(failed1, failed2, failed3)) {
             Optional.ofNullable(art.get()).ifPresent(Throwable::printStackTrace);
             Assert.assertNull(art.get());
-        }
-    }
-
-    @Test(timeout = 5000)
-    public void full() throws InterruptedException {
-        RingBuffer<AtomicInteger> rb = new RingBuffer<>(10, AtomicInteger.class, AtomicInteger::new, i -> i.set(Integer.MIN_VALUE));
-        for (AtomicInteger i = new AtomicInteger(0); i.get() < 16; i.incrementAndGet()) {
-            Assert.assertTrue(rb.put(ai -> ai.set(i.get())));
-        }
-        for (int i = 0; i < 16; i++ ) {
-            Integer io = rb.take(ai -> ai.get());
-            Assert.assertEquals(i, io.intValue());
         }
     }
 
