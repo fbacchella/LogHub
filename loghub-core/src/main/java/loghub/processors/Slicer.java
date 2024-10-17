0f91000e0a603780b479af19a340790671d15485
Fabrice Bacchella <fabrice.bacchella@orange.fr>
2024-10-17 19:11:38 +0200

    A slicer to split an event with a big array in sub array.

---
+++ loghub-core/src/main/java/loghub/processors/Slicer.java
@@ -0,0 +1,115 @@
+package loghub.processors;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Stream;
+
+import loghub.BuilderClass;
+import loghub.TrashedEventException;
+import loghub.Expression;
+import loghub.IgnoredEventException;
+import loghub.PriorityBlockingQueue;
+import loghub.Processor;
+import loghub.ProcessorException;
+import loghub.VariablePath;
+import loghub.configuration.Properties;
+import loghub.events.Event;
+import lombok.Setter;
+
+@BuilderClass(Slicer.Builder.class)
+public class Slicer extends Processor {
+
+    private static final AtomicLong unicityProvider = new AtomicLong();
+
+    @Setter
+    public static class Builder extends Processor.Builder<Slicer> {
+        private Expression bucket = new Expression("unicityProvider", ed -> unicityProvider.incrementAndGet());
+        private VariablePath toSlice;
+        private boolean flatten = false;
+        public Slicer build() {
+            return new Slicer(this);
+        }
+    }
+    public static Slicer.Builder getBuilder() {
+        return new Slicer.Builder();
+    }
+
+    private final Expression bucket;
+    private final VariablePath toSlice;
+    private final boolean flatten;
+    private PriorityBlockingQueue mainQueue;
+
+    public Slicer(Slicer.Builder builder) {
+        this.bucket = builder.bucket;
+        this.toSlice = builder.toSlice;
+        this.flatten = builder.flatten;
+    }
+
+    @Override
+    public boolean configure(Properties properties) {
+        mainQueue = properties.mainQueue;
+        return true;
+    }
+
+    @SuppressWarnings("java:S3776")
+    @Override
+    public boolean process(Event event) throws ProcessorException {
+        Map<Object, Event> events = new LinkedHashMap<>();
+        Stream<Object> enumerator;
+        Object values = event.getAtPath(toSlice);
+        if (values instanceof Collection) {
+            @SuppressWarnings("unchecked")
+            Collection<Object> collection = ((Collection<Object>)values);
+            if (collection.size() <= 1) {
+                throw IgnoredEventException.INSTANCE;
+            } else {
+                enumerator = collection.stream();
+            }
+        } else if (values.getClass().isArray() && Object.class.isAssignableFrom(values.getClass().getComponentType())) {
+            Object[] array = (Object[]) values;
+            if (array.length <= 1) {
+                throw IgnoredEventException.INSTANCE;
+            } else {
+                enumerator = Arrays.stream(array);
+            }
+        } else {
+            throw IgnoredEventException.INSTANCE;
+        }
+        Iterator<Object> iter = enumerator.iterator();
+        while (iter.hasNext()) {
+            Object i = iter.next();
+            event.putAtPath(toSlice, i);
+            Object key = bucket.eval(event.wrap(toSlice));
+            Event newEvent = events.computeIfAbsent(key, k -> buildNewEvent(event));
+            ((List<Object>) newEvent.getAtPath(toSlice)).add(i);
+        }
+        for (Event ev: events.values()) {
+            if (flatten) {
+                List<Object> sub = ((List<Object>) ev.getAtPath(toSlice));
+                if (sub.size() == 1) {
+                    ev.putAtPath(toSlice, sub.get(0));
+                }
+            }
+            ev.reinject(event, mainQueue);
+        }
+        throw TrashedEventException.INSTANCE;
+    }
+
+    private Event buildNewEvent(Event oldEvent) {
+        try {
+            Event newEvent;
+            newEvent = oldEvent.duplicate();
+            newEvent.putAtPath(toSlice, new ArrayList<>());
+            return newEvent;
+        } catch (ProcessorException e) {
+            throw IgnoredEventException.INSTANCE;
+        }
+    }
+
+}
---
+++ loghub-core/src/test/java/loghub/processors/TestSlicer.java
@@ -0,0 +1,115 @@
+package loghub.processors;
+
+import java.beans.IntrospectionException;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import loghub.BeanChecks;
+import loghub.EventsProcessor;
+import loghub.Expression;
+import loghub.LogUtils;
+import loghub.PriorityBlockingQueue;
+import loghub.Processor;
+import loghub.Tools;
+import loghub.VariablePath;
+import loghub.configuration.Properties;
+import loghub.events.Event;
+import loghub.events.EventsFactory;
+import loghub.senders.BlockingConnectionContext;
+
+public class TestSlicer {
+
+    private static final Logger logger = LogManager.getLogger();
+    private final EventsFactory factory = new EventsFactory();
+
+    @BeforeClass
+    public static void configure() throws IOException {
+        Tools.configure();
+        LogUtils.setLevel(logger, Level.TRACE, "loghub.configuration", "loghub.processors.Slicer", "loghub.EventsProcessor");
+    }
+
+    @Test
+    public void testDoNothing() throws IOException, InterruptedException {
+        Consumer<Event> populate = ev -> ev.putAtPath(
+                VariablePath.of("a"), List.of(java.util.Map.of("b", "1"), Map.of("b", "2"))
+        );
+        PriorityBlockingQueue main = run(new StringReader("pipeline[main]{ loghub.processors.Slicer{ toSlice: [b] } }"), populate);
+        Assert.assertTrue(main.isEmpty());
+    }
+
+    @Test
+    public void testSingle() throws IOException, InterruptedException {
+        Consumer<Event> populate = ev -> ev.putAtPath(
+                VariablePath.of("a"), List.of(java.util.Map.of("b", "1"), Map.of("b", "2"))
+        );
+        PriorityBlockingQueue main = run(new StringReader("pipeline[main]{ loghub.processors.Slicer{ toSlice: [a], bucket: now, flatten: true } }"), populate);
+        Event ev1 = main.poll();
+        Assert.assertEquals("1", ev1.getAtPath(VariablePath.of("a", "b")));
+        Event ev2 = main.poll();
+        Assert.assertEquals("2", ev2.getAtPath(VariablePath.of("a", "b")));
+    }
+
+    @Test
+    public void testSingleDefault() throws IOException, InterruptedException {
+        Consumer<Event> populate = ev -> ev.putAtPath(
+                VariablePath.of("a"), List.of(java.util.Map.of("b", "1"), Map.of("b", "2"))
+        );
+        PriorityBlockingQueue main = run(new StringReader("pipeline[main]{ loghub.processors.Slicer{ toSlice: [a] } }"), populate);
+        Event ev1 = main.poll();
+        Assert.assertEquals(List.of(Map.of("b", "1")), ev1.getAtPath(VariablePath.of("a")));
+        Event ev2 = main.poll();
+        Assert.assertEquals(List.of(Map.of("b", "2")), ev2.getAtPath(VariablePath.of("a")));
+    }
+
+    @Test
+    public void testMany() throws IOException, InterruptedException {
+        Consumer<Event> populate = ev -> ev.putAtPath(
+                VariablePath.of("a"), List.of(java.util.Map.of("b", 1), Map.of("b", 2), Map.of("b", 3), Map.of("b", 4))
+        );
+        PriorityBlockingQueue main = run(new StringReader("pipeline[main]{ loghub.processors.Slicer{ toSlice: [a], bucket: [b] % 2 } }"), populate);
+        Event ev1 = main.poll();
+        Assert.assertEquals(List.of(Map.of("b", 1), Map.of("b", 3)), ev1.getAtPath(VariablePath.of("a")));
+        Event ev2 = main.poll();
+        Assert.assertEquals(List.of(Map.of("b", 2), Map.of("b", 4)), ev2.getAtPath(VariablePath.of("a")));
+    }
+
+    public PriorityBlockingQueue run(Reader r, Consumer<Event> populate) throws IOException, InterruptedException {
+        BlockingConnectionContext ctx = new BlockingConnectionContext();
+        Properties props = Tools.loadConf(r);
+        EventsProcessor ep = new EventsProcessor(props.mainQueue, props.outputQueues, props.namedPipeLine, 100, props.repository);
+        ep.start();
+        Event ev = factory.newEvent(ctx);
+        populate.accept(ev);
+        ev.inject(props.namedPipeLine.get("main"), props.mainQueue, true);
+        boolean computed = ctx.getLocalAddress().tryAcquire(5, TimeUnit.DAYS);
+        Assert.assertTrue(computed);
+        ep.interrupt();
+        return props.mainQueue;
+    }
+
+    @Test
+    public void test_loghub_processors_Slicer() throws IntrospectionException, ReflectiveOperationException {
+        BeanChecks.beansCheck(logger, "loghub.processors.Slicer"
+                , BeanChecks.BeanInfo.build("bucket", Expression.class)
+                , BeanChecks.BeanInfo.build("toSlice", VariablePath.class)
+                , BeanChecks.BeanInfo.build("path", VariablePath.class)
+                , BeanChecks.BeanInfo.build("if", Expression.class)
+                , BeanChecks.BeanInfo.build("success", Processor.class)
+                , BeanChecks.BeanInfo.build("failure", Processor.class)
+                , BeanChecks.BeanInfo.build("exception", Processor.class)
+        );
+    }
+
+}
