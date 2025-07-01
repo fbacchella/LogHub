468d169b006a1b52168b5bb28e5585f6de195fea
Fabrice Bacchella <fabrice.bacchella@orange.fr>
2025-07-01 22:47:31 +0200

    Improved DurationConvert processor.

--- loghub-core/src/main/java/loghub/processors/DurationConvert.java
+++ loghub-core/src/main/java/loghub/processors/DurationConvert.java
@@ -1,9 +1,11 @@
 package loghub.processors;
 
+import java.time.DateTimeException;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 
 import loghub.BuilderClass;
+import loghub.IgnoredEventException;
 import loghub.ProcessorException;
 import loghub.events.Event;
 import lombok.Setter;
@@ -90,6 +92,25 @@ public class DurationConvert extends FieldsProcessor {
             public Number to(Duration p) {
                 return p.getSeconds() * 1.0 + (double) p.toNanosPart() / 1_000_000_000L;
             }
+        },
+        DURATION {
+            @Override
+            public Duration from(Number n) {
+                throw new UnsupportedOperationException("Can't parse a number");
+            }
+            public Number to(Duration p) {
+                throw new UnsupportedOperationException("Can't output number");
+            }
+        },
+        STRING {
+            @Override
+            public Duration from(Number n) {
+                throw new UnsupportedOperationException("Can't parse a number");
+            }
+            @Override
+            public Number to(Duration p) {
+                throw new UnsupportedOperationException("Can't output number");
+            }
         };
         public abstract Duration from(Number n);
         public abstract Number to(Duration p);
@@ -118,27 +139,46 @@ public class DurationConvert extends FieldsProcessor {
 
     @Override
     public Object fieldFunction(Event event, Object value) throws ProcessorException {
+        // input value was expected to be a duration
+        if (in == DurationUnit.DURATION && ! (value instanceof Duration)) {
+            throw IgnoredEventException.INSTANCE;
+        }
         Duration d;
         if (value instanceof Number) {
             d = in.from((Number) value);
         } else if (value instanceof String) {
-            Number n;
-            try {
-                n = Long.parseLong((String) value);
-            } catch (NumberFormatException e) {
+            if (in == DurationUnit.STRING) {
                 try {
-                    n = Double.parseDouble((String) value);
-                } catch (NumberFormatException e2) {
-                    throw event.buildException("Can't scan period " + value, e);
+                    d = Duration.parse((String) value);
+                } catch (DateTimeException | ArithmeticException e) {
+                    throw event.buildException("Can't parse duration " + value, e);
                 }
+            } else {
+                Number n;
+                try {
+                    n = Long.parseLong((String) value);
+                } catch (NumberFormatException e) {
+                    try {
+                        n = Double.parseDouble((String) value);
+                    } catch (NumberFormatException e2) {
+                        throw event.buildException("Can't parse duration " + value, e);
+                    }
+                }
+                d = in.from(n);
             }
-            d = in.from(n);
         } else if (value instanceof Duration) {
+            // Can't take Period any way, conversions fails
             d = (Duration) value;
         } else {
-            throw event.buildException("Can't scan period " + value);
+            throw event.buildException("Can't resolve period " + value);
         }
-        return out.to(d);
+        if (out == DurationUnit.DURATION) {
+            return d;
+        } else if (out == DurationUnit.STRING) {
+            return d.toString();
+        } else {
+            return out.to(d);
+        }
     }
 
 }
--- loghub-core/src/test/java/loghub/processors/TestDurationConvert.java
+++ loghub-core/src/test/java/loghub/processors/TestDurationConvert.java
@@ -11,6 +11,7 @@ import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import loghub.IgnoredEventException;
 import loghub.LogUtils;
 import loghub.ProcessorException;
 import loghub.Tools;
@@ -21,11 +22,13 @@ import loghub.events.EventsFactory;
 
 import static loghub.processors.DurationConvert.DurationUnit.CENTI;
 import static loghub.processors.DurationConvert.DurationUnit.DECI;
+import static loghub.processors.DurationConvert.DurationUnit.DURATION;
 import static loghub.processors.DurationConvert.DurationUnit.MICRO;
 import static loghub.processors.DurationConvert.DurationUnit.MILLI;
 import static loghub.processors.DurationConvert.DurationUnit.NANO;
 import static loghub.processors.DurationConvert.DurationUnit.SECOND;
 import static loghub.processors.DurationConvert.DurationUnit.SECOND_FLOAT;
+import static loghub.processors.DurationConvert.DurationUnit.STRING;
 
 public class TestDurationConvert {
 
@@ -71,14 +74,19 @@ public class TestDurationConvert {
         Assert.assertEquals(100L, process(1, DECI, MILLI));
         Assert.assertEquals(1_000_000L, process(1, MILLI, NANO));
         Assert.assertEquals(1_000_000L, process(Duration.ofMillis(1), MILLI, NANO));
+        Assert.assertEquals(100L, process(Duration.ofMillis(100), DURATION, MILLI));
+        Assert.assertEquals(Duration.ofMillis(1), process(1, MILLI, DURATION));
+        Assert.assertEquals(Duration.parse("PT20.345S"), process("PT20.345S", STRING, DURATION));
+        Assert.assertEquals("PT20.345S", process(Duration.parse("PT20.345S"), STRING, STRING));
     }
 
     @Test
     public void testFailure() {
         ProcessorException ex = Assert.assertThrows(ProcessorException.class, () -> process("a", MILLI, NANO));
-        Assert.assertEquals("Field with path \"[field]\" invalid: Can't scan period a", ex.getMessage());
+        Assert.assertEquals("Field with path \"[field]\" invalid: Can't parse duration a", ex.getMessage());
         ProcessorException ex2 = Assert.assertThrows(ProcessorException.class, () -> process(Instant.ofEpochSecond(0), MILLI, NANO));
-        Assert.assertEquals("Field with path \"[field]\" invalid: Can't scan period 1970-01-01T00:00:00Z", ex2.getMessage());
+        Assert.assertEquals("Field with path \"[field]\" invalid: Can't resolve period 1970-01-01T00:00:00Z", ex2.getMessage());
+        Assert.assertThrows(IgnoredEventException.class, () -> process(Instant.ofEpochSecond(0), DURATION, NANO));
     }
 
 }
