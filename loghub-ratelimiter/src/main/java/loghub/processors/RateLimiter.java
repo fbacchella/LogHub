a35581f53cf01c34aa9f5d2117734d066b4c46d0
Fabrice Bacchella <fabrice.bacchella@orange.fr>
2025-09-02 09:50:37 +0200

    The RateLimiter can now be configured to drop events instead of blocking.

--- loghub-ratelimiter/src/main/java/loghub/processors/RateLimiter.java
+++ loghub-ratelimiter/src/main/java/loghub/processors/RateLimiter.java
@@ -5,9 +5,11 @@ import java.time.Duration;
 import javax.cache.Cache;
 
 import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.BlockingBucket;
 import io.github.bucket4j.Bucket;
 import io.github.bucket4j.BucketConfiguration;
 import io.github.bucket4j.distributed.proxy.ProxyManager;
+import io.github.bucket4j.distributed.proxy.RemoteBucketBuilder;
 import io.github.bucket4j.grid.jcache.JCacheProxyManager;
 import io.github.bucket4j.local.SynchronizationStrategy;
 import loghub.BuilderClass;
@@ -21,11 +23,16 @@ import lombok.Setter;
 @BuilderClass(RateLimiter.Builder.class)
 public class RateLimiter extends Processor {
 
+    private interface RateLimiting {
+        boolean limit(Event ev) throws ProcessorException;
+    }
+
     @Setter
     public static class Builder extends Processor.Builder<RateLimiter> {
         private Expression key = null;
         private long rate;
         private long burstRate = -1;
+        private boolean dropping = false;
         private CacheManager cacheManager = new CacheManager(getClass().getClassLoader());
         public RateLimiter build() {
             return new RateLimiter(this);
@@ -35,14 +42,14 @@ public class RateLimiter extends Processor {
         return new RateLimiter.Builder();
     }
 
-    private final Expression keyExpression;
-    private final Bucket singleBucket;
-    private final ProxyManager<Object> buckets;
-    private final BucketConfiguration configuration;
+    private final RateLimiting consumer;
 
     public RateLimiter(Builder builder) {
-        keyExpression = builder.key;
-        if (builder.burstRate < builder.rate && builder.burstRate > 0) {
+        Bucket singleBucket;
+        ProxyManager<Object> buckets;
+        BucketConfiguration configuration;
+        RemoteBucketBuilder<Object> bucketsBuilder;
+        if (builder.burstRate > 0 && builder.burstRate < builder.rate) {
             throw new IllegalArgumentException("Burst rate must be superior to rate");
         }
         Bandwidth limit;
@@ -57,7 +64,7 @@ public class RateLimiter extends Processor {
                             .refillGreedy(builder.rate, Duration.ofSeconds(1))
                             .build();
         }
-        if (keyExpression != null) {
+        if (builder.key != null) {
             configuration = BucketConfiguration.builder()
                                                .addLimit(limit)
                                                .build();
@@ -66,30 +73,70 @@ public class RateLimiter extends Processor {
                                                  .setName("RateLimiter", this)
                                                  .build();
             buckets = new JCacheProxyManager<>(cache);
+            bucketsBuilder = buckets.builder();
             singleBucket = null;
         } else {
             singleBucket = Bucket.builder()
                                  .withSynchronizationStrategy(SynchronizationStrategy.LOCK_FREE)
                                  .addLimit(limit)
                                  .build();
-            buckets = null;
             configuration = null;
+            bucketsBuilder = null;
         }
+        int filter = (builder.dropping ? 1 : 0) | ((builder.key != null ? 1 : 0) << 1);
+        consumer = switch (filter) {
+        case 0 -> { // blocking and no key
+            BlockingBucket bb = singleBucket.asBlocking();
+            yield e -> processBlockNoKey(bb);
+        }
+        case 1 -> { // dropping and no key
+            Drop dropper = new Drop();
+            yield e -> processDropNoKey(e, singleBucket, dropper);
+        }
+        case 2 -> { // blocking and key
+            Expression keyExpression = builder.key;
+            yield e -> processBlockWithKey(e, bucketsBuilder, configuration, keyExpression);
+        }
+        case 3 -> { // dropping and key
+            Drop dropper = new Drop();
+            Expression keyExpression = builder.key;
+            yield e -> processDropWithKey(e, bucketsBuilder, dropper, configuration, keyExpression);
+        }
+        default ->
+            throw new IllegalStateException("Unreachable code");
+        };
     }
 
     @Override
     public boolean process(Event event) throws ProcessorException {
-        Bucket currentBucket;
-        if (singleBucket != null) {
-            currentBucket = singleBucket;
-        } else {
-            currentBucket = buckets.builder().build(keyExpression.eval(event), () -> configuration);
-        }
+        return consumer.limit(event);
+    }
+
+    private boolean processBlockNoKey(BlockingBucket currentBucket) {
         try {
-            currentBucket.asBlocking().consume(1);
+            currentBucket.consume(1);
+            return true;
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
             return false;
+        }
+    }
+
+    private boolean processBlockWithKey(Event event, RemoteBucketBuilder<Object> bucketsBuilder, BucketConfiguration configuration, Expression keyExpression) throws ProcessorException {
+        Bucket currentBucket = bucketsBuilder.build(keyExpression.eval(event), () -> configuration);
+        // Don't really bother with caching asBlocking(), it return this
+        return processBlockNoKey(currentBucket.asBlocking());
+    }
+
+    private boolean processDropWithKey(Event event, RemoteBucketBuilder<Object> bucketsBuilder, Drop dropper, BucketConfiguration configuration, Expression keyExpression)
+            throws ProcessorException {
+        Bucket currentBucket = bucketsBuilder.build(keyExpression.eval(event), () -> configuration);
+        return processDropNoKey(event, currentBucket, dropper);
+    }
+
+    private boolean processDropNoKey(Event e, Bucket currentBucket, Drop dropper) {
+        if (! currentBucket.tryConsume(1)) {
+            e.insertProcessor(dropper);
         }
         return true;
     }
--- loghub-ratelimiter/src/test/java/loghub/processors/TestRateLimiter.java
+++ loghub-ratelimiter/src/test/java/loghub/processors/TestRateLimiter.java
@@ -6,6 +6,7 @@ import java.io.StringReader;
 import java.time.Duration;
 import java.util.Collections;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
 
@@ -16,10 +17,10 @@ import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.codahale.metrics.Timer;
+
 import loghub.BeanChecks;
-import loghub.ConnectionContext;
 import loghub.EventsProcessor;
-import loghub.Expression;
 import loghub.LogUtils;
 import loghub.Pipeline;
 import loghub.ThreadBuilder;
@@ -28,11 +29,16 @@ import loghub.configuration.ConfigException;
 import loghub.configuration.Properties;
 import loghub.events.Event;
 import loghub.events.EventsFactory;
+import loghub.metrics.Stats;
 
 public class TestRateLimiter {
 
     private static Logger logger;
-    private final EventsFactory factory = new EventsFactory();
+    private static final EventsFactory factory = new EventsFactory();
+    private static final Duration testDuration = Duration.ofSeconds(5);
+
+    private final AtomicInteger sent = new AtomicInteger();
+    private final AtomicInteger received = new AtomicInteger();
 
     @BeforeClass
     public static void configure() {
@@ -41,9 +47,14 @@ public class TestRateLimiter {
         LogUtils.setLevel(logger, Level.TRACE, "loghub.processors.RateLimiter");
     }
 
-    private int runFlow(Duration testDuration, Consumer<RateLimiter.Builder> configurator) throws IOException, InterruptedException {
-        Properties props = Tools.loadConf(new StringReader("queueDepth: 1 queueWeight: 2"));
-        AtomicInteger ai = new AtomicInteger();
+    private void withinPercentage(int expected, int actual, double percent) {
+        double lower = expected * (1 - percent / 100.0);
+        double upper = expected * (1 + percent / 100.0);
+        Assert.assertTrue("Expecting %d, got %d".formatted(expected, actual),actual >= lower && actual <= upper);
+    }
+
+    private void runFlow(Consumer<RateLimiter.Builder> configurator, int expected) throws IOException, InterruptedException {
+        Properties props = Tools.loadConf(new StringReader("queueDepth: 1 queueWeight: 2 pipeline[main] {} output $main | {}"));
         RateLimiter.Builder builder = RateLimiter.getBuilder();
         configurator.accept(builder);
         RateLimiter rl = builder.build();
@@ -51,40 +62,77 @@ public class TestRateLimiter {
         Pipeline pipe = new Pipeline(Collections.singletonList(rl), "main", null);
         Thread t = ThreadBuilder.get().setTask(() -> {
             while (!Thread.interrupted()) {
-                Event ev = factory.newEvent(ConnectionContext.EMPTY);
-                ev.put("count", ai.incrementAndGet());
+                Event ev = factory.newEvent();
                 ev.inject(pipe, props.mainQueue, true);
+                sent.incrementAndGet();
             }
-        }).build(true);
+        }).setVirtual(true).build(true);
+
+        Thread consumer = ThreadBuilder.get().setTask(() -> {
+            try {
+                while (!Thread.interrupted()) {
+                    props.outputQueues.get("main").poll(100, TimeUnit.MILLISECONDS);
+                    received.incrementAndGet();
+                }
+            } catch (InterruptedException e) {
+                // just exit
+            }
+        }).setVirtual(true).build(true);
 
         Map<String, Pipeline> namedPipeLine = Collections.singletonMap("main,", pipe);
         EventsProcessor ep = new EventsProcessor(props.mainQueue, props.outputQueues, namedPipeLine, 100, props.repository);
         ep.start();
         Thread.sleep(testDuration.toMillis());
         t.interrupt();
+        consumer.interrupt();
         ep.interrupt();
-        return ai.get();
+        t.join();
+        consumer.join();
+        ep.join();
+        logger.debug("Send: {}", sent::get);
+        logger.debug("Received: {}", received::get);
+        logger.debug("Dropped: {}", Stats::getDropped);
+        logger.debug("Processed in pipeline main: {}", () -> Stats.getMetric(Timer.class, "main", "timer").getCount());
+        withinPercentage(expected, received.get() / (int) testDuration.toSeconds(), 5);
+        Assert.assertTrue(Integer.toString(sent.get() - (received.get() + (int) Stats.getDropped())), (sent.get() - (received.get() + Stats.getDropped())) < 5);
     }
 
     @Test
-    public void testRate() throws InterruptedException, ConfigException, IOException {
-        Duration testDuration = Duration.ofSeconds(5);
-        int countRate = runFlow(testDuration, b -> b.setRate(100));
-        int countBurstRate = runFlow(testDuration, b -> {
+    public void testBaseRate() throws InterruptedException, ConfigException, IOException {
+        Stats.reset();
+        runFlow(b -> b.setRate(100), 120);
+        logger.debug("Nominal requested rate is {}", () -> (float) received.get() / testDuration.toSeconds());
+        Assert.assertEquals(0, Stats.getDropped());
+    }
+
+    @Test
+    public void testBurstRate() throws InterruptedException, ConfigException, IOException {
+        Stats.reset();
+        runFlow(b -> {
             b.setRate(100);
             b.setBurstRate(200);
-        });
-        logger.debug("Nominal requested rate is {}", () -> (float) countRate /testDuration.toSeconds());
-        logger.debug("Burst requested rate is {}", () -> (float) countBurstRate /testDuration.toSeconds());
-        Assert.assertTrue("Got " + ((float)countRate / testDuration.toSeconds()) + " event/s", countRate < countBurstRate);
+        }, 140);
+        logger.debug("Burst requested rate is {}", () -> (float) received.get() /testDuration.toSeconds());
+        Assert.assertEquals(0, Stats.getDropped());
+    }
+
+    @Test
+    public void testRateDropping() throws InterruptedException, ConfigException, IOException {
+        Stats.reset();
+        runFlow(b -> {
+            b.setRate(100);
+            b.setDropping(true);
+        }, 120);
+        logger.debug("Dropping requested rate is {}", () -> (float) received.get() / testDuration.toSeconds());
+        Assert.assertNotEquals(0, Stats.getDropped());
     }
 
     @Test
     public void testBeans() throws IntrospectionException, ReflectiveOperationException {
         BeanChecks.beansCheck(logger, "loghub.processors.RateLimiter"
-                              , BeanChecks.BeanInfo.build("rate", Long.TYPE)
-                              , BeanChecks.BeanInfo.build("burstRate", Long.TYPE)
-                              , BeanChecks.BeanInfo.build("key", Expression.class)
+                              , BeanChecks.BeanInfo.build("rate", long.class)
+                              , BeanChecks.BeanInfo.build("burstRate", long.class)
+                              , BeanChecks.BeanInfo.build("dropping", boolean.class)
         );
     }
 
