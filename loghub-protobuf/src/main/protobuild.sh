4f348073978576bd0d944386f8636cec8cb44703
Fabrice Bacchella <fabrice.bacchella@orange.fr>
2026-03-06 17:50:04 +0100

    Improved handling and compilation of protobuf .protof files

--- loghub-protobuf/src/main/protobuild.sh
+++ loghub-protobuf/src/main/protobuild.sh
@@ -1,14 +1,31 @@
 set -x
 chmod a+rx "$PROTOC_BIN"
 mkdir -p "$DESCRIPTOR_OUT_DIR"
-for f in $PROTO_SRC_DIR/* ; do
-    name=$(basename "$f" .proto)
-    dir=$(dirname "$f")
-    "$PROTOC_BIN" \
-        --descriptor_set_out="$DESCRIPTOR_OUT_DIR/${name}.binpb" \
-        --include_imports \
-        --proto_path="$PROTO_SRC_DIR" \
-        -I$PROTOC_INCLUDES \
-        -I$PROTO_SRC_DIR \
-         $(find $f -name '*.proto')
+for name in prometheus prometheus2 opentelemetry centreon ping ; do
+    case $name in
+    prometheus)
+        finddir=prometheus
+        ;;
+    opentelemetry)
+        finddir=opentelemetry
+        ;;
+    prometheus2)
+        finddir=io/prometheus
+        ;;
+    centreon)
+        finddir=com/centreon
+        ;;
+    ping)
+        finddir=ping
+        ;;
+    esac
+    if [ -e "$PROTO_SRC_DIR/$finddir" ] ; then
+        "$PROTOC_BIN" \
+            --descriptor_set_out="$DESCRIPTOR_OUT_DIR/${name}.binpb" \
+            --include_imports \
+            --proto_path="$PROTO_SRC_DIR" \
+            -I$PROTOC_INCLUDES \
+            -I$PROTO_SRC_DIR \
+             $(find $PROTO_SRC_DIR/$finddir -name '*.proto')
+    fi
 done
