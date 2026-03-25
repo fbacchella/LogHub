afca03f45bc7ff69ac3fa3120dcb88622eb821d6
Fabrice Bacchella <fabrice.bacchella@orange.fr>
2026-03-25 11:23:27 +0100

    Adding an UUID generator as a processor, for maximum configuration possibilities.

---
+++ loghub-uuid/src/main/java/loghub/processors/UuidProcessor.java
@@ -0,0 +1,181 @@
+package loghub.processors;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.time.Instant;
+import java.util.Random;
+import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
+
+import com.github.f4b6a3.uuid.UuidCreator;
+import com.github.f4b6a3.uuid.enums.UuidLocalDomain;
+import com.github.f4b6a3.uuid.enums.UuidNamespace;
+import com.github.f4b6a3.uuid.factory.standard.NameBasedMd5Factory;
+import com.github.f4b6a3.uuid.factory.standard.NameBasedSha1Factory;
+import com.github.f4b6a3.uuid.factory.standard.RandomBasedFactory;
+
+import loghub.BuilderClass;
+import loghub.Expression;
+import loghub.Processor;
+import loghub.ProcessorException;
+import loghub.VariablePath;
+import loghub.events.Event;
+import lombok.Setter;
+
+@BuilderClass(UuidProcessor.Builder.class)
+public class UuidProcessor extends Processor {
+
+    public enum Version {
+        V1,
+        V1_MAC,
+        V1_HASH,
+        V1_RANDOM,
+        V1_PROVIDED,
+        V2,
+        V3,
+        V3_CUSTOM,
+        V4,
+        V4_FAST,
+        V4_SECURE,
+        V5,
+        V5_CUSTOM,
+        V6,
+        V7,
+        V7_TYPE1,
+        V7_TYPE2,
+        V7_TYPE3;
+    }
+
+    @FunctionalInterface
+    public static interface UuidGenerator {
+        UUID generate(Event event) throws ProcessorException;
+    }
+
+    @Setter
+    public static class Builder extends Processor.Builder<UuidProcessor> {
+        private Version version = Version.V7;
+        private Expression name;
+        private Expression localNumber;
+        private Expression nodeIdentifier;
+        private UuidNamespace namespace;
+        private String namespaceUuid;
+        private UuidLocalDomain localDomain;
+        private String randomSource;
+        private VariablePath field = VariablePath.of("message");
+
+        @Override
+        public UuidProcessor build() {
+            return new UuidProcessor(this);
+        }
+    }
+
+    public static Builder getBuilder() {
+        return new Builder();
+    }
+
+    private final UuidGenerator generator;
+    private final VariablePath field;
+
+    private UuidProcessor(Builder builder) {
+        super(builder);
+        this.generator = getGenerator(builder);
+        this.field = builder.field;
+    }
+
+    private UuidGenerator getGenerator(Builder builder) {
+        return switch (builder.version) {
+            case V1 -> getGeneratorV1();
+            case V1_MAC -> event -> UuidCreator.getTimeBasedWithMac();
+            case V1_HASH -> event -> UuidCreator.getTimeBasedWithHash();
+            case V1_RANDOM -> event -> UuidCreator.getTimeBasedWithRandom();
+            case V1_PROVIDED -> getGeneratorV1(builder.nodeIdentifier);
+            case V2 -> getGeneratorV2(builder.localDomain, builder.localNumber);
+            case V3 -> getGeneratorV3(builder.namespace, builder.name);
+            case V3_CUSTOM -> getGeneratorV3(builder.namespaceUuid, builder.name);
+            case V4, V4_FAST -> event -> UuidCreator.getRandomBasedFast();
+            case V4_SECURE -> getGeneratorV4(builder.randomSource);
+            case V5 -> getGeneratorV5(builder.namespace, builder.name);
+            case V5_CUSTOM -> getGeneratorV5(builder.namespaceUuid, builder.name);
+            case V6 -> event -> UuidCreator.getTimeOrdered();
+            case V7, V7_TYPE1 -> event -> UuidCreator.getTimeOrderedEpoch();
+            case V7_TYPE2 -> event -> UuidCreator.getTimeOrderedEpochPlus1();
+            case V7_TYPE3 -> event -> UuidCreator.getTimeOrderedEpochPlusN();
+        };
+    }
+
+    private UuidGenerator getGeneratorV1() {
+        long l = ThreadLocalRandom.current().nextLong();
+        return event -> UuidCreator.getTimeBased(Instant.now(), null, l);
+    }
+
+    private UuidGenerator getGeneratorV1(Expression value) {
+        return event -> {
+            Object v = value.eval(event);
+            if (v instanceof Number n) {
+                return UuidCreator.getTimeBased(Instant.now(), null, n.longValue());
+            } else {
+                throw event.buildException("Expected number for time based UUID generation");
+            }
+        };
+    }
+
+    private UuidGenerator getGeneratorV2(UuidLocalDomain domain, Expression value) {
+        return event -> {
+            Object v = value.eval(event);
+            if (v instanceof Number n) {
+                return UuidCreator.getDceSecurity(domain, n.intValue());
+            } else {
+                throw event.buildException("Expected number for DCE Security UUID generation");
+            }
+        };
+    }
+
+    private UuidGenerator getGeneratorV3(UuidNamespace namespace, Expression value) {
+        NameBasedMd5Factory factory = new NameBasedMd5Factory(namespace);
+        return event -> factory.create(getName(value, event));
+    }
+
+    private UuidGenerator getGeneratorV3(String namespace, Expression value) {
+        UUID namespaceUuid = UUID.fromString(namespace);
+        NameBasedMd5Factory factory = new NameBasedMd5Factory(namespaceUuid);
+        return event -> factory.create(getName(value, event));
+    }
+
+    private UuidGenerator getGeneratorV4(String secureRandom) {
+        try {
+            Random random = SecureRandom.getInstance(secureRandom);
+            RandomBasedFactory factory = new RandomBasedFactory(random);
+            return event -> factory.create();
+        } catch (NoSuchAlgorithmException e) {
+            throw new IllegalArgumentException("Unknown secure random", e);
+        }
+    }
+
+    private UuidGenerator getGeneratorV5(UuidNamespace namespace, Expression value) {
+        NameBasedSha1Factory factory = new NameBasedSha1Factory(namespace);
+        return event -> factory.create(getName(value, event));
+    }
+
+    private UuidGenerator getGeneratorV5(String namespace, Expression value) {
+        UUID namespaceUuid = UUID.fromString(namespace);
+        NameBasedSha1Factory factory = new NameBasedSha1Factory(namespaceUuid);
+        return event -> factory.create(getName(value, event));
+    }
+
+    @Override
+    public boolean process(Event event) throws ProcessorException {
+        UUID uuid = generator.generate(event);
+        event.putAtPath(field, uuid);
+        return true;
+    }
+
+    private String getName(Expression value, Event event) throws ProcessorException {
+        Object result = value.eval(event);
+        if (result == null) {
+            throw event.buildException("Cannot generate UUID: value is null");
+        } else {
+            return result.toString();
+        }
+    }
+
+}
---
+++ loghub-uuid/src/test/java/loghub/processors/TestUuidProcessor.java
@@ -0,0 +1,145 @@
+package loghub.processors;
+
+import java.beans.IntrospectionException;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.Collections;
+import java.util.UUID;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+import com.github.f4b6a3.uuid.UuidCreator;
+import com.github.f4b6a3.uuid.enums.UuidLocalDomain;
+import com.github.f4b6a3.uuid.enums.UuidNamespace;
+
+import loghub.BeanChecks;
+import loghub.Expression;
+import loghub.Processor;
+import loghub.ProcessorException;
+import loghub.Tools;
+import loghub.VariablePath;
+import loghub.configuration.Configuration;
+import loghub.configuration.Properties;
+import loghub.events.Event;
+import loghub.events.EventsFactory;
+import loghub.processors.UuidProcessor.Version;
+
+class TestUuidProcessor {
+
+    private static final Logger logger = LogManager.getLogger();
+    private final EventsFactory factory = new EventsFactory();
+
+    @BeforeAll
+    static void configure() {
+        Tools.configure();
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = Version.class)
+    void testVersions(Version version) throws ProcessorException {
+        UuidProcessor.Builder builder = UuidProcessor.getBuilder();
+        builder.setField(VariablePath.of("uuid"));
+        builder.setVersion(version);
+
+        switch (version) {
+            case V3, V5 -> {
+                builder.setName(new Expression("github.fr"));
+                builder.setNamespace(UuidNamespace.NAMESPACE_DNS);
+            }
+            case V3_CUSTOM, V5_CUSTOM -> {
+                builder.setName(new Expression("github.fr"));
+                builder.setNamespaceUuid(UuidNamespace.NAMESPACE_DNS.getValue().toString());
+            }
+            case V1_PROVIDED -> builder.setNodeIdentifier(new Expression(1));
+            case V2 -> {
+                builder.setLocalDomain(UuidLocalDomain.LOCAL_DOMAIN_PERSON);
+                builder.setLocalNumber(new Expression(1));
+            }
+            case V4_SECURE -> builder.setRandomSource("SHA1PRNG");
+            default -> { }
+        }
+
+        UuidProcessor uuid = builder.build();
+        Assertions.assertTrue(uuid.configure(new Properties(Collections.emptyMap())));
+
+        Event event = factory.newEvent();
+        event.put("f1", "test value");
+        Assertions.assertTrue(uuid.process(event));
+        UUID result = (UUID) event.get("uuid");
+        Assertions.assertNotNull(result);
+
+        int expectedVersion = switch (version) {
+            case V1, V1_MAC, V1_HASH, V1_RANDOM, V1_PROVIDED -> 1;
+            case V2 -> 2;
+            case V3, V3_CUSTOM -> 3;
+            case V4, V4_FAST, V4_SECURE -> 4;
+            case V5, V5_CUSTOM -> 5;
+            case V6 -> 6;
+            case V7, V7_TYPE1, V7_TYPE2, V7_TYPE3 -> 7;
+        };
+        Assertions.assertEquals(expectedVersion, result.version());
+
+        if (version == Version.V3) {
+            UUID refuuid = UuidCreator.getNameBasedMd5(UuidNamespace.NAMESPACE_DNS, "github.fr");
+            Assertions.assertEquals(refuuid, result);
+        } else if (version == Version.V5) {
+            UUID refuuid = UuidCreator.getNameBasedSha1(UuidNamespace.NAMESPACE_DNS, "github.fr");
+            Assertions.assertEquals(refuuid, result);
+        }
+    }
+
+    @Test
+    void testInvalidSecureRandom() {
+        UuidProcessor.Builder builder = UuidProcessor.getBuilder();
+        builder.setVersion(Version.V4_SECURE);
+        builder.setRandomSource("InvalidSecureRandom");
+        Assertions.assertThrows(IllegalArgumentException.class, builder::build);
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = Version.class)
+    void testParsing(Version version) throws IOException {
+        String confile = """
+            pipeline[defaultmessage] {
+                loghub.processors.UuidProcessor {
+                    version: "%s",
+                    namespace: "NAMESPACE_DNS",
+                    namespaceUuid: "4e2b4d4c-92e8-11ed-86a8-3fdb0085247e",
+                    localDomain: "LOCAL_DOMAIN_PERSON",
+                    randomSource: "NativePRNG",
+                    field: [#uuid],
+                    nodeIdentifier: 1,
+                    name: "loghub.com",
+                    localNumber: 1,
+                }
+            }""".formatted(version);
+        Properties p =  Configuration.parse(new StringReader(confile));
+        Event ev = factory.newEvent();
+        Tools.runProcessing(ev, p.namedPipeLine.get("defaultmessage"), p);
+        Assertions.assertInstanceOf(UUID.class, ev.getAtPath(VariablePath.ofMeta("uuid")));
+    }
+
+    @Test
+    void testBeans() throws IntrospectionException, ReflectiveOperationException {
+        BeanChecks.beansCheck(logger, "loghub.processors.UuidProcessor"
+                , BeanChecks.BeanInfo.build("version", Version.class)
+                , BeanChecks.BeanInfo.build("name", Expression.class)
+                , BeanChecks.BeanInfo.build("nodeIdentifier", Expression.class)
+                , BeanChecks.BeanInfo.build("namespace", UuidNamespace.class)
+                , BeanChecks.BeanInfo.build("namespaceUuid", String.class)
+                , BeanChecks.BeanInfo.build("localDomain", UuidLocalDomain.class)
+                , BeanChecks.BeanInfo.build("randomSource", String.class)
+                , BeanChecks.BeanInfo.build("field", VariablePath.class)
+                , BeanChecks.BeanInfo.build("if", Expression.class)
+                , BeanChecks.BeanInfo.build("success", Processor.class)
+                , BeanChecks.BeanInfo.build("failure", Processor.class)
+                , BeanChecks.BeanInfo.build("exception", Processor.class)
+        );
+    }
+}
