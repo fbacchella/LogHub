45f0113236635c3e69b8f22ff92a27743f0f75c7
Fabrice Bacchella <fabrice.bacchella@orange.fr>
2026-04-16 21:20:43 +0200

    Fixing TlsCipers processor.

--- loghub-core/src/main/java/loghub/processors/TlsCiphers.java
+++ loghub-core/src/main/java/loghub/processors/TlsCiphers.java
@@ -8,11 +8,30 @@ import java.nio.charset.StandardCharsets;
 import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import loghub.BuilderClass;
 import loghub.events.Event;
 import lombok.Setter;
 
+/**
+ * This processor translates TLS cipher suite names between different contexts (IANA, OpenSSL, GnuTLS, Java).
+ * It uses the hexadecimal identifier of the cipher suite as a pivot for translation.
+ * <p>
+ * The underlying YAML resource files are derived from
+ * <a href="https://github.com/hcrudolph/ciphersuite.info/">https://github.com/hcrudolph/ciphersuite.info/</a>.
+ * <p>
+ *     It also use the content of https://www.iana.org/assignments/tls-parameters/tls-parameters.xhtml#tls-parameters-4
+ * </p>
+ * <p>
+ *     The file openssldump.txtopenssldump.txt was generated using the command {@code openssl ciphers -V 'ALL:@SECLEVEL=0'}
+ * </p>
+ * <p>
+ *     The GnuTLS ciphers name can be found at <a href="https://www.gnutls.org/manual/html_node/Supported-ciphersuites.html">Appendix D Supported Ciphersuites</a>
+ * </p>
+ */
 @BuilderClass(TlsCiphers.Builder.class)
 public class TlsCiphers extends FieldsProcessor {
 
@@ -37,9 +56,17 @@ public class TlsCiphers extends FieldsProcessor {
         return new Builder();
     }
 
+    private static final Pattern CSV_PATTERN = Pattern.compile("^\"(0x[0-9A-Fa-f]{2}),(0x[0-9A-Fa-f]{2})\",(?<name>[^,]+),.*$");
+    private static final Pattern OPENSSLDUMP_PATTERN = Pattern.compile("^\\s*(?<hex1>0x[0-9A-Fa-f]{2}),(?<hex2>0x[0-9A-Fa-f]{2})\\s*-\\s*(?<name>\\S+)\\s+.*$");
+
     private final Map<String, String> translationMap;
 
-    private record CipherId(byte byte1, byte byte2) {}
+    record CipherId(byte byte1, byte byte2) {
+        @Override
+        public String toString() {
+            return "0x%02x,0x%02x".formatted(byte1, byte2);
+        }
+    }
 
     private TlsCiphers(Builder builder) {
         super(builder);
@@ -79,6 +106,63 @@ public class TlsCiphers extends FieldsProcessor {
             } catch (IOException e) {
                 logger.error("Failed to load cipher resource {}: {}", resource, e.getMessage());
             }
+        }
+
+        loadIanaCsvResource(idToNames, contextToNameToId);
+        loadOpenSslDumpResource(idToNames, contextToNameToId);
+    }
+
+    private void loadIanaCsvResource(Map<CipherId, Map<Context, String>> idToNames, Map<Context, Map<String, CipherId>> contextToNameToId) {
+        String resource = "/ciphers/tls-parameters-4.csv";
+        try (InputStream is = getClass().getResourceAsStream(resource)) {
+            if (is == null) {
+                logger.error("Resource not found: {}", resource);
+                return;
+            }
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8))) {
+                String line;
+                // Skip header
+                reader.readLine();
+                Set<String> keywords = Set.of("Reserved", "Unassigned");
+                while ((line = reader.readLine()) != null) {
+                    Matcher m = CSV_PATTERN.matcher(line);
+                    if (m.matches()) {
+                        String hex1 = m.group(1);
+                        String hex2 = m.group(2);
+                        String name = m.group("name");
+                        if (keywords.contains(name)) {
+                            continue;
+                        }
+                        store(Context.IANA, name, hex1, hex2, idToNames, contextToNameToId);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            logger.error("Failed to load cipher resource {}: {}", resource, e.getMessage());
+        }
+    }
+
+    private void loadOpenSslDumpResource(Map<CipherId, Map<Context, String>> idToNames, Map<Context, Map<String, CipherId>> contextToNameToId) {
+        String resource = "/ciphers/openssldump.txt";
+        try (InputStream is = getClass().getResourceAsStream(resource)) {
+            if (is == null) {
+                logger.error("Resource not found: {}", resource);
+                return;
+            }
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    Matcher m = OPENSSLDUMP_PATTERN.matcher(line);
+                    if (m.matches()) {
+                        String hex1 = m.group("hex1");
+                        String hex2 = m.group("hex2");
+                        String name = m.group("name");
+                        store(Context.OPENSSL, name, hex1, hex2, idToNames, contextToNameToId);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            logger.error("Failed to load cipher resource {}: {}", resource, e.getMessage());
         }
     }
 
@@ -148,4 +232,17 @@ public class TlsCiphers extends FieldsProcessor {
         String translated = translationMap.get(value.toString());
         return translated != null ? translated : RUNSTATUS.FAILED;
     }
+
+    /**
+     * Used for test, not for generic use
+     * @return
+     */
+    static Map<CipherId, Map<Context, String>> resolveId() {
+        TlsCiphers cipher = getBuilder().build();
+        Map<CipherId, Map<Context, String>> idToNames = new HashMap<>();
+        Map<Context, Map<String, CipherId>> contextToNameToId = new EnumMap<>(Context.class);
+        cipher.loadResources(idToNames, contextToNameToId);
+        return idToNames;
+    }
+
 }
--- loghub-core/src/test/java/loghub/processors/TestTlsCiphers.java
+++ loghub-core/src/test/java/loghub/processors/TestTlsCiphers.java
@@ -1,6 +1,15 @@
 package loghub.processors;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.net.ssl.SSLContext;
 
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
@@ -10,13 +19,14 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import loghub.LogUtils;
+import loghub.ProcessorException;
 import loghub.Tools;
 import loghub.VariablePath;
 import loghub.configuration.Properties;
 import loghub.events.Event;
 import loghub.events.EventsFactory;
-
-import loghub.ProcessorException;
+import loghub.processors.TlsCiphers.CipherId;
+import loghub.processors.TlsCiphers.Context;
 
 class TestTlsCiphers {
 
@@ -33,8 +43,7 @@ class TestTlsCiphers {
     @Test
     void testTranslation() throws ProcessorException {
         TlsCiphers.Builder builder = TlsCiphers.getBuilder();
-        builder.setSourceContext("iana");
-        builder.setDestinationContext("openssl");
+        builder.setDestinationContext(TlsCiphers.Context.OPENSSL);
         TlsCiphers processor = builder.build();
         processor.setField(VariablePath.parse("cipher"));
         Assertions.assertTrue(processor.configure(new Properties(Collections.emptyMap())));
@@ -45,13 +54,25 @@ class TestTlsCiphers {
         processor.process(event);
         // OpenSSL name is the same for this one in 02_openssl_ciphers.yaml
         Assertions.assertEquals("TLS_AES_256_GCM_SHA384", event.get("cipher"));
+
+        // Test a cipher ONLY present in the CSV: TLS_ASCONAEAD128_ASCONHASH256 (0x00, 0x6E)
+        event = factory.newEvent();
+        event.put("cipher", "TLS_ASCONAEAD128_ASCONHASH256");
+        processor.process(event);
+        // Should return the name itself if no OpenSSL translation is found
+        Assertions.assertEquals("TLS_ASCONAEAD128_ASCONHASH256", event.get("cipher"));
+
+        // Test the first one in the CSV: TLS_NULL_WITH_NULL_NULL (0x00, 0x00)
+        event = factory.newEvent();
+        event.put("cipher", "TLS_NULL_WITH_NULL_NULL");
+        processor.process(event);
+        Assertions.assertEquals("TLS_NULL_WITH_NULL_NULL", event.get("cipher"));
     }
 
     @Test
     void testTranslationJava() throws ProcessorException {
         TlsCiphers.Builder builder = TlsCiphers.getBuilder();
-        builder.setSourceContext("iana");
-        builder.setDestinationContext("java");
+        builder.setDestinationContext(TlsCiphers.Context.JAVA);
         TlsCiphers processor = builder.build();
         processor.setField(VariablePath.parse("cipher"));
         Assertions.assertTrue(processor.configure(new Properties(Collections.emptyMap())));
@@ -64,4 +85,121 @@ class TestTlsCiphers {
         // In 04_java: TLS_RSA_WITH_AES_128_CBC_SHA
         Assertions.assertEquals("TLS_RSA_WITH_AES_128_CBC_SHA", event.get("cipher"));
     }
+
+    @Test
+    void testAutoDetection() throws ProcessorException {
+        TlsCiphers.Builder builder = TlsCiphers.getBuilder();
+        builder.setDestinationContext(TlsCiphers.Context.IANA);
+        TlsCiphers processor = builder.build();
+        processor.setField(VariablePath.parse("cipher"));
+        Assertions.assertTrue(processor.configure(new Properties(Collections.emptyMap())));
+
+        Event event = factory.newEvent();
+        // RSA_WITH_AES_128_CBC_SHA is OpenSSL name for TLS_RSA_WITH_AES_128_CBC_SHA
+        event.put("cipher", "AES128-SHA"); // OpenSSL name
+        processor.process(event);
+        Assertions.assertEquals("TLS_RSA_WITH_AES_128_CBC_SHA", event.get("cipher"));
+    }
+
+    @Test
+    void testOpensslCiphers() throws Exception {
+        ProcessBuilder pb = new ProcessBuilder("openssl", "ciphers", "-V");
+        Process p = pb.start();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()))) {
+            String line;
+            Pattern linePattern = Pattern.compile("\\s+0x([0-9A-F]{2}),0x([0-9A-F]{2}) - (\\S+).*");
+            TlsCiphers.Builder builder = TlsCiphers.getBuilder();
+            builder.setDestinationContext(TlsCiphers.Context.IANA);
+            TlsCiphers processor = builder.build();
+            processor.setField(VariablePath.parse("cipher"));
+            processor.configure(new Properties(Collections.emptyMap()));
+
+            int count = 0;
+            while ((line = reader.readLine()) != null) {
+                Matcher m = linePattern.matcher(line);
+                if (m.matches()) {
+                    count++;
+                    String opensslName = m.group(3);
+                    Event event = factory.newEvent();
+                    event.put("cipher", opensslName);
+                    processor.process(event);
+                    Assertions.assertNotEquals(FieldsProcessor.RUNSTATUS.FAILED, event.get("cipher"), "Failed to translate OpenSSL cipher: " + opensslName);
+                }
+            }
+            logger.info("Validated {} ciphers from openssl ciphers -V", count);
+        }
+    }
+
+    @Test
+    void testJavaCiphers() throws Exception {
+        SSLContext context = SSLContext.getDefault();
+        String[] cipherSuites = context.getSupportedSSLParameters().getCipherSuites();
+
+        TlsCiphers.Builder builder = TlsCiphers.getBuilder();
+        builder.setDestinationContext(TlsCiphers.Context.IANA);
+        TlsCiphers processor = builder.build();
+        processor.setField(VariablePath.parse("cipher"));
+        processor.configure(new Properties(Collections.emptyMap()));
+
+        int count = 0;
+        for (String cipherSuite : cipherSuites) {
+            // Skip SCSV ciphers as they are not real ciphers
+            if (cipherSuite.contains("_SCSV")) {
+                continue;
+            }
+            count++;
+            Event event = factory.newEvent();
+            event.put("cipher", cipherSuite);
+            processor.process(event);
+            Assertions.assertNotEquals(FieldsProcessor.RUNSTATUS.FAILED, event.get("cipher"), "Failed to translate Java cipher: " + cipherSuite);
+        }
+        logger.info("Validated {} ciphers from Java JSSE", count);
+    }
+
+    @Test
+    void testOpensslDump() throws ProcessorException {
+        TlsCiphers.Builder builder = TlsCiphers.getBuilder();
+        builder.setDestinationContext(TlsCiphers.Context.IANA);
+        TlsCiphers processor = builder.build();
+        processor.setField(VariablePath.parse("cipher"));
+        Assertions.assertTrue(processor.configure(new Properties(Collections.emptyMap())));
+
+        Event event = factory.newEvent();
+        // TLS_CHACHA20_POLY1305_SHA256 is in openssldump.txt as 0x13,0x03
+        // Its IANA name is also TLS_CHACHA20_POLY1305_SHA256
+        event.put("cipher", "TLS_CHACHA20_POLY1305_SHA256");
+        processor.process(event);
+        Assertions.assertEquals("TLS_CHACHA20_POLY1305_SHA256", event.get("cipher"));
+
+        // ECDHE-RSA-AES256-GCM-SHA384 (0xC0,0x30)
+        event = factory.newEvent();
+        event.put("cipher", "ECDHE-RSA-AES256-GCM-SHA384");
+        processor.process(event);
+        Assertions.assertEquals("TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384", event.get("cipher"));
+    }
+
+    @Test
+    void testYamlConsistency() {
+        Map<CipherId, Map<Context, String>> contextToIdToName = TlsCiphers.resolveId();
+
+        for (Map.Entry<CipherId, Map<Context, String>> entry : contextToIdToName.entrySet()) {
+            if (entry.getValue().size() == 4) {
+                continue;
+            }
+            CipherId id = entry.getKey();
+            Map<Context, String> currentIdToName = entry.getValue();
+            if (!currentIdToName.containsKey(Context.IANA)) {
+                logger.warn("Missing IANA key {} with names {}", id, currentIdToName);
+            } else {
+                Set<Context> missing = HashSet.newHashSet(4);
+                for (Context c: Context.values()) {
+                    if (! entry.getValue().containsKey(c)) {
+                        missing.add(c);
+                    }
+                }
+                logger.warn("Missing {} for {}/{}", missing, currentIdToName.get(Context.IANA), id);
+            }
+        }
+    }
+
 }
